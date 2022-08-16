f8c7645a65d40097dc163f1608b1f39f6eb66017
Israel Antezana Rojas <israelantezana@gmail.com>
2022-08-16 16:59:43 -0400

    Form for Sumador

--- src/Sumador.js
+++ src/Sumador.js
@@ -1,5 +1,15 @@
 function Sumador() {
-  return <div className="Sumador">Sumador</div>;
+  return (
+    <div className="Sumador">
+      <h3>Sumador</h3>
+      <form>
+        <label for="numero1">Numero 1:</label>
+        <input type="number" id="numero1"></input>
+        <label for="numero2">Numero 2:</label>
+        <input type="number" id="numero2"></input>
+        <button id="sumar-btn">Sumar</button>
+      </form>
+    </div>
+  );
 }
-
 export default Sumador;
