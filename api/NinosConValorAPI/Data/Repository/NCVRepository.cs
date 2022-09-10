edc0d121876e56b87b3c8e58acef8a15877bfb4b
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-09-09 23:53:32 -0400

    fixedAsset Service Added

---
+++ api/NinosConValorAPI/Data/Repository/NCVRepository.cs
@@ -0,0 +1,31 @@
+﻿using NinosConValorAPI.Data.Entity;
+
+namespace NinosConValorAPI.Data.Repository
+{
+    public class NCVRepository:INCVRepository
+    {
+        private NCV_DBContext _dbContext;
+        public NCVRepository(NCV_DBContext NCV_DBContext)
+        {
+            _dbContext = NCV_DBContext;
+        }
+
+        public void CreateFixedAsset(FixedAssetEntity fixedAsset)
+        {
+            _dbContext.FixedAssets.Add(fixedAsset);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            try
+            {
+                var result = await _dbContext.SaveChangesAsync();
+                return result > 0 ? true : false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
