807f59608d7051aa2ba97a4d7f533cf4c17e5a76
miky14n <miky14n@gmail.com>
2022-09-10 19:13:27 -0400

    Correccion de error en la prueba de llamada al Home Page

--- web-ui/cypress/e2e/smoke.cy.js
+++ web-ui/cypress/e2e/smoke.cy.js
@@ -1,6 +1,6 @@
 describe('Smoke test', () => {
   it('muestra la pagina principal', () => {
     cy.visit('/');
-    cy.contains('Sumador');
+    cy.contains('Niños con Valor org');
   });
 });
--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,6 +1,6 @@
 //import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import SumadorForm from './SumadorForm';
+import SumadorForm from './SumadorForm';
 import HomePageForm from './HomePageForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
