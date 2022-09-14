b5bd80d261a538e77e0f4badbb56ef4aa147bf64
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-09-14 19:38:21 -0400

    test to pass data between pages

--- web-ui/src/kidsFilesView/DataHealth.js
+++ web-ui/src/kidsFilesView/DataHealth.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useLocation } from "react-router-dom";
 
-function DataHealth(){
+function DataHealth(idKid){
+    const location = useLocation();
+    console.log( " parameter inside dataHealth: " , idKid  )
+    console.log("LOCATion: ", location )
+    const data = location.state?.data;
     return(
         <div>
             <h1> Welcome in data health</h1>
+            <h2> id is {data ? data.id: "que?"  } </h2>
         </div>
     )
 }
