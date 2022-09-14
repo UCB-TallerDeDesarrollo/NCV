d979f253cf41e70af030c1558e2399e9926049f7
Diego Quiñones <diegoqr2000@gmail.com>
2022-09-14 09:57:49 -0400

    Pequeños cambios en el index para login

--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom/client';
 import SumadorForm from './SumadorForm';
 import HomePageForm from './HomePageForm';
+import LoginForm from './components/LoginForm';
 import CrearActivoFijoForm from './components/CrearActivoFijoForm';
 import App from './App';
 
