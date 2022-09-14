6c43303bda197be14b582b331459bef1be5476ec
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-09-14 19:38:21 -0400

    test to pass data between pages

--- web-ui/src/AppRoutes.js
+++ web-ui/src/AppRoutes.js
@@ -3,16 +3,20 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
 import HomePageForm from './HomePageForm';  
 import DataHealth from "./kidsFilesView/DataHealth";
-  
+import ViewListKids from "./kidsFilesView/temporaryViewListKids";  
+
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePageForm />} />
-        <Route path="kidHealth/*" element={<DataHealth />} />
+        <Route path="kidHealth" element={<DataHealth />} />
+        <Route path="listKids" element={<ViewListKids />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
   );
