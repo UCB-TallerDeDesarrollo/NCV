cf726d5eafbc71cebe0f797aa609be77d834f3a1
Kevin <kevcespedesarancibia87@gmail.com>
2022-09-14 20:22:08 -0400

    KidControllerUT not finished test commented

--- api/UnitTests/ControllersUT/KidControllerUT.cs
+++ api/UnitTests/ControllersUT/KidControllerUT.cs
@@ -18,29 +18,29 @@ namespace UnitTests.ControllersUT
     public class KidControllerUT
     {
 
-        [Fact]
-        public async Task GetKid_ReturnStatusFixedAsset()
-        {
-            var kid1 = new KidModel()
-            {
-                Id = 1,
-                FirstName = "Manuel",
-                LastName = "Flores",
-                CI = "1234567",
-                BirthDate = new DateTime(2010,9,12),
-                ProgramHouse = "SDA",
-                BirthPlace = "Cochabamba",
-                Gender="masculino"
-            };
+        //[Fact]
+        //public async Task GetKid_ReturnStatusFixedAsset()
+        //{
+        //    var kid1 = new KidModel()
+        //    {
+        //        Id = 1,
+        //        FirstName = "Manuel",
+        //        LastName = "Flores",
+        //        CI = "1234567",
+        //        BirthDate = new DateTime(2010,9,12),
+        //        ProgramHouse = "SDA",
+        //        BirthPlace = "Cochabamba",
+        //        Gender="masculino"
+        //    };
 
-            var kidServiceMock = new Mock<IKidService>();
-            kidServiceMock.Setup(r => r.GetKidAsync(1)).ReturnsAsync(kid1);
+        //    var kidServiceMock = new Mock<IKidService>();
+        //    kidServiceMock.Setup(r => r.GetKidAsync(1)).ReturnsAsync(kid1);
 
-            var kidController = new KidsController(kidServiceMock.Object);
-            var response = await kidController.GetKidAync(1);
-            var status = (OkObjectResult)response.Result;
-            Assert.Equal(200, status.StatusCode);
+        //    var kidController = new KidsController(kidServiceMock.Object);
+        //    var response = await kidController.GetKidAync(1);
+        //    var status = (OkObjectResult)response.Result;
+        //    Assert.Equal(200, status.StatusCode);
 
-        }
+        //}
     }
 }
