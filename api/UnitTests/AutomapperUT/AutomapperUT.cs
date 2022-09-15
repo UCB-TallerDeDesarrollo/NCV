4d8bd638bb66115e45fcfa2d529be8ef927d4537
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-09-15 01:27:10 -0400

    Autommapper_Model_Entity_Probado

--- api/UnitTests/AutomapperUT/AutomapperUT.cs
+++ api/UnitTests/AutomapperUT/AutomapperUT.cs
@@ -142,6 +142,37 @@ namespace UnitTests.AutomapperUT
             //Assert
             Assert.Equal(typefixedAssetModelType, actualType);
         }
-        
+        // KID
+        [Fact]
+        public void AutomapperProfile_KidModel_ReturnsKidEntity()
+        {
+            // ARRANGE
+            var kidModel = new KidModel()
+            {
+                Id = 1,
+                FirstName = "Manuel",
+                LastName = "Flores",
+                CI = "1234567",
+                BirthDate = new DateTime(2010, 9, 12),
+                ProgramHouse = "SDA",
+                BirthPlace = "Cochabamba",
+                Gender = "masculino"
+            };
+
+            KidEntity kidEntity = new KidEntity();
+            var typeOfKidEntity = kidEntity.GetType().ToString();
+
+
+            // ACT
+            var actual = _mapper.Map<KidEntity>(kidModel);
+            var actualType = actual.GetType().ToString();
+
+            // ASSERT
+            Assert.Equal(typeOfKidEntity, actualType);
+            Assert.Equal(kidModel.CI, actual.CI);
+            Assert.Equal(kidModel.Id, actual.Id);
+
+        }
+
     }
 }
