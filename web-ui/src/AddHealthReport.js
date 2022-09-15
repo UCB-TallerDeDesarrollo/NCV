5cd36a44763eb1993f9ec991c8597f1f89a54db2
OmarArias <omarxarias@gmail.com>
2022-09-14 21:26:20 -0400

    Formulario inicial
    Formulario con los campos puestos en la base de datos inicial (Sin fetch)

---
+++ web-ui/src/AddHealthReport.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+const healtReport = {
+  BloodType: "",
+  CIDiscapacidad: "",
+  PsychologicalDiagnosis: "",
+  NeurologicalDiagnosis: "",
+  SpecialDiagnosis: "",
+  HealthProblems:"",
+};
+
+function  AddHealthReport (){
+    
+  const [formReport, setformReport] = useState(healtReport);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setformReport({
+      ...formReport,
+      [name]: value,
+    });
+  };
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log("Subiendo...");
+    console.log(formReport);
+}
+
+  return (
+      <Box
+        component="form"
+        sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        >
+        <div>
+            <TextField
+                required
+                id="bloodtype"
+                name="BloodType"
+                label="Tipo de Sangre"
+                type="text"
+                value={formReport.BloodType}
+                onChange={handleInputChange}
+            />
+
+            <TextField
+                required
+                id="CIDiscapacidad"
+                name="CIDiscapacidad"
+                label="CI Discapacidad"
+                type="text"
+                value={formReport.CIDiscapacidad}
+                onChange={handleInputChange}
+            />
+
+            <TextField
+                id="PsychologicalDiagnosis"
+                name="PsychologicalDiagnosis"
+                label="Diagnostico Fisico"
+                helperText="Opcional"
+                value={formReport.PsychologicalDiagnosis}
+                onChange={handleInputChange}
+            />
+
+            <TextField
+                id="NeurologicalDiagnosis"
+                name="NeurologicalDiagnosis"
+                label="Diagnostico Neurologico"
+                helperText="Opcional"
+                value={formReport.NeurologicalDiagnosis}
+                onChange={handleInputChange}
+            />
+
+            <TextField
+                id="SpecialDiagnosis"
+                name="SpecialDiagnosis"
+                label="Diagnostico Especial"
+                helperText="Opcional"
+                value={formReport.SpecialDiagnosis}
+                onChange={handleInputChange}
+            />
+
+            <TextField
+                id="HealthProblems"
+                name="HealthProblems"
+                label="Problemas de Salud"
+                helperText="Opcional"
+                value={formReport.HealthProblems}
+                onChange={handleInputChange}
+            />
+        </div>
+
+        <Button variant="text" onClick={handleFormSubmit}>Guardar</Button>
+        </Box>
+  );
+};
+export default AddHealthReport;
--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -2,6 +2,8 @@
 import ReactDOM from 'react-dom/client';
 import SumadorForm from './SumadorForm';
 import HomePageForm from './HomePageForm';
+import AddHealthReport from './AddHealthReport';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HomePageForm />);
+root.render(<AddHealthReport />);
