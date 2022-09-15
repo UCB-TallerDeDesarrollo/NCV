e2524e1154ad759a75fe2fe3cd6721877db383f3
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-09-15 00:24:50 -0400

    MostrarFile_ControllerProbado

--- api/UnitTests/ControllersUT/KidsControllerUT.cs
+++ api/UnitTests/ControllersUT/KidsControllerUT.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using NinosConValorAPI;
 using NinosConValorAPI.Controllers;
+using NinosConValorAPI.Data.Entity;
+using NinosConValorAPI.Data.Repository;
 using NinosConValorAPI.Models;
 using NinosConValorAPI.Services;
 using System;
@@ -8,11 +12,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTests.NCVRepositoryUT;
 
 namespace UnitTests.ControllersUT
 {
-    internal class KidsControllerUT
+    public class KidsControllerUT
     {
 
+        [Fact]
+        public async Task GetKidAsync_SingleKid_ReturnStatus()
+        {
+            var kidModel = new KidModel()
+            {
+                Id = 1,
+                FirstName = "Manuel",
+                LastName = "Flores",
+                CI = "1234567",
+                BirthDate = new DateTime(2010, 9, 12),
+                ProgramHouse = "SDA",
+                BirthPlace = "Cochabamba",
+                Gender = "masculino"
+            };
+            int kidId = 1;
+            var kidServiceMock = new Mock<IKidService>();
+            kidServiceMock.Setup(r => r.GetKidAsync(kidId)).ReturnsAsync(kidModel);
+
+            var kidController = new KidsController(kidServiceMock.Object);
+            var response = await kidController.GetKidAync(kidId);
+            var status = (OkObjectResult)response.Result;
+            Assert.Equal(200, status.StatusCode);
+        }
     }
 }
