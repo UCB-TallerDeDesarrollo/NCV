34cb5d209a64b1e60732c9eac21137526677bc44
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-09-15 01:17:40 -0400

    added route to healthView

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,Navigate } from 'react-router-dom';
 import HomePageForm from './HomePageForm';
 import CrearActivoFijoForm from './components/CrearActivoFijoForm';
 import LoginForm from './components/LoginForm';
+import DataHealth from './kidsFilesView/DataHealth';
 
 function App(){
   return(
@@ -11,6 +12,8 @@ function App(){
         <Route path='/crear-activo-fijo' element={<CrearActivoFijoForm/>}></Route>
         <Route path='/home-ncv' element={<HomePageForm/>}></Route>
         <Route exact path='/' element={<LoginForm/>}></Route>
+        <Route path="kidHealth" element={<DataHealth />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </Router>
   );
