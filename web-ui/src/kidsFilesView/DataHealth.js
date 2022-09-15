f898f541ad5d348597cb637807102ec5e47a2c86
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-09-15 15:26:35 -0400

    added basic configuration of routing

---
+++ web-ui/src/kidsFilesView/DataHealth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DataHealth(){
+    return(
+        <div>
+            <h1> Welcome in data health</h1>
+        </div>
+    )
+}
+
+export default DataHealth;
