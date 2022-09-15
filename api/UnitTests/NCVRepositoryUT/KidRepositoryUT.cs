462fc1772d5bd2f62be91f0add0fc4c4d7b2ba18
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-09-15 02:48:49 -0400

    KidRepository_CreateFile_Probado

--- api/UnitTests/NCVRepositoryUT/KidRepositoryUT.cs
+++ api/UnitTests/NCVRepositoryUT/KidRepositoryUT.cs
@@ -1,4 +1,5 @@
-﻿using NinosConValorAPI.Data.Entity;
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NinosConValorAPI.Data.Entity;
 using NinosConValorAPI.Data.Repository;
 using System;
 using System.Collections.Generic;
@@ -38,6 +39,29 @@ namespace UnitTests.NCVRepositoryUT
             // ASSERT
             Assert.Equal(1, kidEntity.Id);
             Assert.Equal(kidId, kidEntity.Id);
+        }
+        [Fact]
+        public async Task CreateKid_AddKidToEmptyDB_ReturnsTrueSavedCorrectly()
+        {
+            // ARRANGE
+            var repository = new NCVRepository(ctx);
+            var kid = new KidEntity()
+            {
+                FirstName = "Manuel",
+                LastName = "Flores",
+                CI = "1234567",
+                BirthDate = new DateTime(2010, 9, 12),
+                ProgramHouse = "SDA",
+                BirthPlace = "Cochabamba",
+                Gender = "masculino"
+            };
+
+            // ACT 
+            repository.CreateKid(kid);
+            var result = await repository.SaveChangesAsync();
+
+            // ASSERT
+            Assert.True(result);
         }
     }
 }
