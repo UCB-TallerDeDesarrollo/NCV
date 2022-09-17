9ac979f84ff8fdb7e278f6fe26620de9639a762d
Andres <53712714+Andres-Pereira@users.noreply.github.com>
2022-09-17 12:47:54 -0400

    Correccion rebase

--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,6 +1,8 @@
-// import React from 'react';
 import ReactDOM from 'react-dom/client'
 import HomePageForm from './HomePageForm'
+import LoginForm from './components/LoginForm'
+import CrearActivoFijoForm from './components/CrearActivoFijoForm'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<HomePageForm />)
+root.render(<App/>)
