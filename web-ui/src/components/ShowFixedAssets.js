439ac30bde32cdd7852e241e26467e7b72d2095a
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-09-23 09:24:16 -0400

    Added navigation from showFixedAssets

--- web-ui/src/components/ShowFixedAssets.js
+++ web-ui/src/components/ShowFixedAssets.js
@@ -11,69 +11,99 @@ import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import { useNavigate } from 'react-router-dom'
 
-export default function ShowFixedAssets () {
-  const [fixedAssets, setFixedAssets] = React.useState(null)
+export default function ShowFixedAssets() {
+    const navigate = useNavigate()
+    const completeInfoFixedAsset = '/activos-fijos/'
+    const createFixedAssetRoute = '/crear-activo-fijo'
+    const [fixedAssets, setFixedAssets] = React.useState(null)
 
-  useEffect(() => {
-    axios
-      .get('https://ncv-api.herokuapp.com/api/fixedAssets')
-      .then(response => {
-        setFixedAssets(response.data)
-        console.log(response.data)
-      })
-  }, [])
+    useEffect(() => {
+        axios
+            .get('https://ncv-api.herokuapp.com/api/fixedAssets')
+            .then((response) => {
+                setFixedAssets(response.data)
+                console.log(response.data)
+            })
+    }, [])
 
-  if (!fixedAssets) return null
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          ></IconButton>
-        </Toolbar>
-      </AppBar>
-      <div style={{ marginLeft: '7%' }}>
+    if (!fixedAssets) return null
+    return (
         <div>
-          <h1>ACTIVOS FIJOS</h1>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    ></IconButton>
+                </Toolbar>
+            </AppBar>
+            <div style={{ marginLeft: '7%' }}>
+                <div>
+                    <h1>ACTIVOS FIJOS</h1>
+                    <Button
+                        variant="outlined"
+                        onClick={(e) => navigate(createFixedAssetRoute)}
+                    >
+                        Agregar Activo Fijo
+                    </Button>
+                </div>
+                {fixedAssets.map((asset) => {
+                    return (
+                        <Grid
+                            id="lista-activos-fijos"
+                            style={{ minHeight: '60vh' }}
+                        >
+                            <Card sx={{ p: 2, maxWidth: 1300 }} key={asset.id}>
+                                <Box sx={{ display: 'flex' }}>
+                                    <CardHeader id="Name" title={asset.name} />
+                                </Box>
+                                <Box sx={{ display: 'inline-block' }}>
+                                    <CardMedia
+                                        component="img"
+                                        image="https://st.depositphotos.com/1005574/2080/v/450/depositphotos_20808761-stock-illustration-laptop.jpg"
+                                        sx={{ width: 400 }}
+                                    ></CardMedia>
+                                </Box>
+                                <Box
+                                    sx={{
+                                        display: 'inline-block',
+                                        position: 'absolute',
+                                        right: '30%'
+                                    }}
+                                >
+                                    <CardContent>
+                                        <h4>
+                                            Descripción: {asset.description}{' '}
+                                        </h4>
+                                        <h4>
+                                            Fecha de entrada: {asset.entryDate.split('T')[0]}{' '}
+                                        </h4>
+                                        <h4>Precio: {asset.price} </h4>
+                                        <br></br>
+                                        <Button
+                                            variant="outlined"
+                                            onClick={(e) =>
+                                                navigate(
+                                                    completeInfoFixedAsset +
+                                                        asset.id
+                                                )
+                                            }
+                                        >
+                                            Ver más
+                                        </Button>
+                                    </CardContent>
+                                </Box>
+                            </Card>
+                        </Grid>
+                    )
+                })}
+            </div>
         </div>
-        {fixedAssets.map(asset => {
-          return (
-            <Grid id="lista-activos-fijos" style={{ minHeight: '60vh' }} key={asset.id}>
-              <Card sx={{ p: 2, maxWidth: 1300 }}>
-                <Box sx={{ display: 'flex' }}>
-                  <CardHeader id="Name" title={asset.name} />
-                </Box>
-                <Box sx={{ display: 'inline-block' }}>
-                  <CardMedia
-                    component="img"
-                    image="https://st.depositphotos.com/1005574/2080/v/450/depositphotos_20808761-stock-illustration-laptop.jpg"
-                    sx={{ width: 400 }}
-                  ></CardMedia>
-                </Box>
-                <Box
-                  sx={{
-                    display: 'inline-block',
-                    position: 'absolute',
-                    right: '30%'
-                  }}
-                >
-                  <CardContent>
-                    <h4>Descripción: {asset.description} </h4>
-                    <h4>Fecha de entrada: {asset.entryDate} </h4>
-                    <h4>Precio: {asset.price} </h4>
-                  </CardContent>
-                </Box>
-              </Card>
-            </Grid>
-          )
-        })}
-      </div>
-    </div>
-  )
+    )
 }
