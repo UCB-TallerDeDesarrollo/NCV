2d93210765f7f35e3e18324aa20116decccab10a
CarolinaMelendez <74866367+CarolinaMelendez@users.noreply.github.com>
2022-09-23 23:15:11 -0400

    Create Logo.jsx

---
+++ web-ui/src/Components/Logo.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+import imgNiñosConValor from '../Assets/img/logo-ncv.png'
+
+// El logo se encuentra en formato png, esta sin fondo
+const Logo = (props) => {
+    return (
+        <Fragment>
+            <img
+                src={imgNiñosConValor}
+                alt="Niños con Valor"
+                className="logo-img"
+            />
+        </Fragment>
+    )
+}
+
+export default Logo
