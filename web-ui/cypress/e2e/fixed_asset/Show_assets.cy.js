96b8bb65dda77093c1470bbaa29d5b47badb071e
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2022-09-27 21:11:33 -0400

    Making Show Assets e2e test use a MOCK API and also adding more tests

--- web-ui/cypress/e2e/fixed_asset/Show_assets.cy.js
+++ web-ui/cypress/e2e/fixed_asset/Show_assets.cy.js
@@ -1,12 +1,26 @@
 describe('Show assets end to end tests', () => {
   it('Muestra la lista de los activos fijos', () => {
+    cy.intercept('GET', 'https://ncv-api.herokuapp.com/api/fixedAssets',{
+      fixture: 'fixedAssets/listOfAssets.json'
+    }).as('listOfAssets',);
+
     cy.visit('/activos-fijos');
 
-    cy.intercept('GET', 'https://ncv-api.herokuapp.com/api/fixedAssets').as(
-      'ListaActivosFijos',
-    );
-    cy.wait('@ListaActivosFijos')
-      .its('response.statusCode')
-      .should('equal', 200);
+    cy.get('.activos-fijos')
+    .should('have.length', 5)
+  });
+  it('Verifica campos de un activo fijo de la lista', () => {
+    cy.intercept('GET', 'https://ncv-api.herokuapp.com/api/fixedAssets',{
+      fixture: 'fixedAssets/listOfAssets.json'
+    }).as('listOfAssets',);
+
+    cy.visit('/activos-fijos');
+
+    cy.get('#lista-activos-fijos')
+    .should('have.length', 1)
+    .and('contain', 'Computadora2')
+    .and('contain', 'Computadora de escritorio')
+    .and('contain', "2022-10-16")
+    .and('contain', 100.58);
   });
 });
