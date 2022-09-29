35a7c5569f6a891c80c4113780e5488cfe9efbdb
Daniel Acero <edanielacero@gmail.com>
2022-09-29 01:28:34 -0400

    addded basic changes

--- web-ui/src/Components/SingleItemCard.jsx
+++ web-ui/src/Components/SingleItemCard.jsx
@@ -5,27 +5,41 @@ import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import Avatar from '@mui/material/Avatar'
 import IconButton, { IconButtonProps } from '@mui/material/IconButton'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
 import Button from '@mui/material/Button'
 import CardContent from '@mui/material/CardContent'
 
-const SingleItemCard = ({about}) => {
-    return (
+const SingleItemCard = ({element}) => {
+    /*return (
         <Card sx={{ maxWidth: 600 }}>
             <CardHeader
                 avatar={
                     <Avatar
-                        alt="Profile Picture"
+                        alt="Picture"
                         src="https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg"
                     />
                 }
-                title={about.Name}
-                subheader={about.description}
+                
+                title={element.Title}
+                subheader={element.description}
             />
             <CardContent>
-                /*AQUI LISTAR EL RESTO DE INFORMACION DEPENDE DE LA LISTA*/
+            for (var prop in element){
+                <div>
+                    <h2>"Key:" + {prop}  </h2>
+                    <h2>"Value:" {element[prop]}</h2>
+                </div>
+            }
             </CardContent>
         </Card>
+    )*/
+    return(
+
+        <div></div>
+
+        /*<div>
+            <h1>{element.firstName}</h1>
+            {element.map(element => <h5 key={element.id} name={element.firstname} />)}
+        </div>*/
     )
 }
 
--- web-ui/src/Views/KidsFiles/temporaryViewListKids.js
+++ web-ui/src/Views/KidsFiles/temporaryViewListKids.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react'
-import { useSearchParams, BrowserRouter, Link } from 'react-router-dom'
+import SingleItemCard from '../../Components/SingleItemCard'
+import { useState, useEffect } from 'react'
 
-function ViewListKids() {
-    const [data, setData] = useState({
-        id: '1245'
-    })
-
+function Temp() {
+    const [kidsList, setKidList] = useState([])
+    const urlKids = 'https://ncv-api.herokuapp.com/api/kids/1'
+    useEffect(() => {
+        fetch(urlKids)
+            .then((res) => res.json())
+            .then(setKidList);
+    }, [])
+    /*let kidsListComponent = null
+    const baseUrl ="/ninos/informacionpersonal"
+    if (kidsList.length>0){
+        const listElements = kidsList.map((el)=>{
+            return {
+                id:el.id, 
+                title:`${el.firstName} ${el.lastName}`, 
+                description:el.ci, 
+                elementUrl:`${baseUrl}/${el.id}`
+            }
+        })
+        kidsListComponent = <SingleItemCard items={listElements} />
+    }*/
     return (
         <div>
-            <Link to="/kidHealth" state={{ data }}>
-                Niño carlos
-            </Link>
+            <SingleItemCard element={kidsList}/>
         </div>
     )
 }
-
-export default ViewListKids
+export default Temp
