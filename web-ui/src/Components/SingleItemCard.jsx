4c99be5d4e7a5114487105d8a643608b904c4ee6
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-09-29 04:00:27 -0400

    Completed basic view of one kid file

--- web-ui/src/Components/SingleItemCard.jsx
+++ web-ui/src/Components/SingleItemCard.jsx
@@ -4,78 +4,44 @@ import React from 'react'
 import Card from '@mui/material/Card'
 import Box from '@mui/material/Box'
 import CardMedia from '@mui/material/CardMedia'
-import Grid from '@mui/material/Grid'
-import CardHeader from '@mui/material/CardHeader'
-import Avatar from '@mui/material/Avatar'
-import IconButton, { IconButtonProps } from '@mui/material/IconButton'
-import Button from '@mui/material/Button'
 import CardContent from '@mui/material/CardContent'
 
+
 const SingleItemCard = ({element}) => {
-    /*return (
-        <Card sx={{ maxWidth: 600 }}>
-            <CardHeader
-                avatar={
-                    <Avatar
-                        alt="Picture"
-                        src="https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg"
-                    />
-                }
-                
-                title={element.Title}
-                subheader={element.description}
-            />
-            <CardContent>
-            for (var prop in element){
+    let imageUrl = "https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg"
+    let detailsElement = []
+      for (const prop in element ){
+        if (prop != "id" ){
+            detailsElement.push(
                 <div>
-                    <h2>"Key:" + {prop}  </h2>
-                    <h2>"Value:" {element[prop]}</h2>
+                    <h4>{prop}:  {element[prop]}</h4><br></br>
                 </div>
-            }
-            </CardContent>
-        </Card>
-    )*/
+            )
+        }
+      } 
+
     return(
-        /*<Grid id="element" class="element" style={{ minHeight: '60vh' }}>
-            <Card sx={{ p: 2, maxWidth: 1300 }} key={element.id}>
-                <Box sx={{ display: 'flex' }}>
-                    <CardHeader id="Name" title="Hola" />
-                </Box>
-                <Box sx={{ display: 'inline-block' }}>
-                    <Avatar alt="Picture" src="https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg" sx={{ width: 400 }} />
-                </Box>
-                <Box sx={{ display: 'inline-block',position: 'absolute',right: '30%'}}>
-                    <CardContent>
-                        <h4> Descripción: {element.ci}{' '}</h4>
-                        <h4> Fecha de entrada: {element.birthDate.split('T')[0]}{' '}</h4>
-                        <h4>Precio: {element.ci} </h4>
-                        <br></br>
-                    </CardContent>
-                </Box>
-            </Card>
-        </Grid>*/
-
-        /*<Card>
-            <Box sx={{ display: 'inline-block' }}>
-                    <Avatar
-                        alt="Picture"
-                        src="https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg"
-                    />
-                <CardContent>
-                    <h1>Hola</h1>
-                    <h1>{element.firstName}</h1>
-                </CardContent>
-            </Box>
-        </Card>*/
-
-        <Card>
-            <Box sx={{ display: 'inline-block' }}>
-                <CardContent>
-                    <h1>Hola</h1>
-                    <h1>{element.firstName}</h1>
-                </CardContent>
-            </Box>
-        </Card>
+        <div>
+                <Card sx={{ p: 5, maxWidth: 1300}}>
+                        <Box sx={{ display: 'inline-block' }} >
+                        <CardMedia
+                            component="img"
+                            image={ imageUrl}
+                            sx={{ width: 400 }}
+                        ></CardMedia>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: 'inline-block',
+                                right: '30%'
+                            }}
+                        >
+                            <CardContent>
+                                {detailsElement}
+                            </CardContent>
+                        </Box>
+                </Card>
+        </div>
     )
 }
 
--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,7 +1,6 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Temp from './Views/KidsFiles/temporaryViewListKids'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Temp/>)
+root.render(<App/>)
