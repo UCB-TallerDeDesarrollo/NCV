717fc34bb790d8e19b0eb8311955f5f717e858a8
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-10-01 15:10:25 -0400

    erased console log from ErrorPagesdfs

--- web-ui/src/Components/ErrorPage.jsx
+++ web-ui/src/Components/ErrorPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 function ErrorPage (error) {
-    console.log(error)
     const message = error.response.data
     const httpStatus = error.response.status
     return <h1>ERROR {httpStatus}: {message}</h1>
