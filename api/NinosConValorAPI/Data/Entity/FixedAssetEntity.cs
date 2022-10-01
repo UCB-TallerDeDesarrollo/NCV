3568cc27087c36c87945a755ea7dff6d64da0c2c
Jav4nsJuice <javans.ferrel@gmail.com>
2022-10-01 00:52:23 -0400

    Solving problems in FixedAssetEntity.cs

--- api/NinosConValorAPI/Data/Entity/FixedAssetEntity.cs
+++ api/NinosConValorAPI/Data/Entity/FixedAssetEntity.cs
@@ -8,7 +8,7 @@ namespace NinosConValorAPI.Data.Entity
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [Required]
         public decimal Price { get; set; }
         [Required]
