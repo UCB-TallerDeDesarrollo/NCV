9bea2a3d8a8ad7d862e800001d9c8ae8bcdd1ba0
Kevin <kevcespedesarancibia87@gmail.com>
2022-10-01 17:23:25 -0400

    Code quality refactor

--- web-ui/src/Components/ListElement.jsx
+++ web-ui/src/Components/ListElement.jsx
@@ -6,17 +6,18 @@ import { useNavigate } from 'react-router-dom';
 
 export default function ListElement({id=0, title = "default title", description = "default description", imgSrc = "", elementUrl = "", withImage=true}){
   const navigate = useNavigate();
-  let img = null;
-  if (withImage)
-    img = <ListItemAvatar>
-      <Avatar alt="Remy Sharp" src={imgSrc}/>
-    </ListItemAvatar>;
-  return <ListItemButton sx={{borderTop: 1, borderColor:'#CDCDCD', margin:0}} key={id} alignItems="flex-start" onClick={()=>navigate(elementUrl)}>
-      {img}
-    <ListItemText primary={title} secondary={description} className="ListElement" sx={{
+  
+  const sxListItemText = {
     '& .MuiListItemText-primary': {
       fontSize: 18,
-    },
-  }}/>
+    }
+  }
+  
+  let img = null;
+  if (withImage)
+    img = <ListItemAvatar> <Avatar alt="Remy Sharp" src={imgSrc}/> </ListItemAvatar>;
+  return <ListItemButton sx={{borderTop: 1, borderColor:'#CDCDCD', margin:0}} key={id} alignItems="flex-start" onClick={()=>navigate(elementUrl)}>
+      {img}
+    <ListItemText primary={title} secondary={description} className="ListElement" sx={sxListItemText}/>
   </ListItemButton>;
 }
