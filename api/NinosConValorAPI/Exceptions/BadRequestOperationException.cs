3881f5b0c3f170e49b18b105a66377b4e4e2ed6a
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-10-01 20:04:59 -0400

    exceptions_backend_kids

--- api/NinosConValorAPI/Exceptions/BadRequestOperationException.cs
+++ api/NinosConValorAPI/Exceptions/BadRequestOperationException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace VideoGameAPI.Exceptions
+namespace NinosConValorAPI.Exceptions
 {
     public class BadRequestOperationException : Exception
     {
