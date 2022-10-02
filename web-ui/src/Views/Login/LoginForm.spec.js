bc4d4b54c4d32fe90455db5a9c66d1ef5f1cf425
Diego Quiñones <diegoqr2000@gmail.com>
2022-10-01 23:32:45 -0400

    Test at backend for Login added

--- web-ui/src/Views/Login/LoginForm.spec.js
+++ web-ui/src/Views/Login/LoginForm.spec.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import LoginForm from './LoginForm';
 import '@testing-library/jest-dom'
 
