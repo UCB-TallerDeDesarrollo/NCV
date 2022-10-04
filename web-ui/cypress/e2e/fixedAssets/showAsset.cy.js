39e7881715832e1824c4a3b8df30a815196c550a
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-10-04 15:05:47 -0400

    modified tests for show one fixed asset

--- web-ui/cypress/e2e/fixedAssets/showAsset.cy.js
+++ web-ui/cypress/e2e/fixedAssets/showAsset.cy.js
@@ -1,14 +1,4 @@
 describe('Show a fixed asset end to end tests', () => {
-  it('Shows a single fixed asset', () => {
-    cy.intercept('GET', 'https://ncv-api.herokuapp.com/api/fixedAssets/9',{
-      fixture: 'fixedAssets/anAsset.json'
-    }).as('getAnAsset',);
-
-    cy.visit('/activos-fijos/9');
-
-    cy.get('li')
-    .should('have.length', 1)
-  });
   it('Verifies the fields from a single fixed asset', () => {
     cy.intercept('GET', 'https://ncv-api.herokuapp.com/api/fixedAssets/9',{
       fixture: 'fixedAssets/anAsset.json'
@@ -16,14 +6,11 @@ describe('Show a fixed asset end to end tests', () => {
 
     cy.visit('/activos-fijos/9');
 
-    cy.get('h4')
-    .should('have.length', 5)
-    .and('contain', 'Computadora de escritorio')
-    .and('contain', 2424.24)
-    .and('contain', '16 GB de Ram con targeta grafica NVIDIA')
-    .and('contain', 3);
-    
-    cy.get('span')
-    .and('contain', 'Computadora cypress');
+    cy.get('div')
+    cy.contains('Computadora cypress')
+    cy.contains('Computadora de escritorio')
+    cy.contains(2424.24)
+    cy.contains('16 GB de Ram con targeta grafica NVIDIA')
+    cy.contains(3)
   });
 });
