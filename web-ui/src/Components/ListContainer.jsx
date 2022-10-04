fa0c19f175cabf0c96017d40418bdec6bc55964c
Kevin <kevcespedesarancibia87@gmail.com>
2022-10-04 01:34:52 -0400

    header added to ListContainer component, Button added to ShowKidsFiles

--- web-ui/src/Components/ListContainer.jsx
+++ web-ui/src/Components/ListContainer.jsx
@@ -1,10 +1,13 @@
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
-
-export default function ListContainer({children,title=""}) {
+import Button from '@mui/material/Button'
+export default function ListContainer({children,title="",header={}}) {
   return (
         <Box sx={{bgcolor:'#ffff', padding:4, margin:3, borderRadius:3, boxShadow: 2, width:0.75}}>
-            <h4>{title}</h4>
+            <div style={{ marginBottom: 10,display:'flex', alignItems:'center', flexDirection:'row', width:'100%',justifyContent:'space-between' }}>
+              <h4 style={{margin:0,padding:0}}>{title}</h4>
+              {header}
+            </div>
             {children}
         </Box>
   );
