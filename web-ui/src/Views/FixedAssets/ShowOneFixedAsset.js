d0f633a88ad8a308eae89c633866c48d4659de66
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-10-04 14:34:55 -0400

    standarized show fixed asset with SingleItemCard component

--- web-ui/src/Views/FixedAssets/ShowOneFixedAsset.js
+++ web-ui/src/Views/FixedAssets/ShowOneFixedAsset.js
@@ -1,71 +1,36 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
-import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
 import getFromApi from '../../Components/GetFromApi'
 import ErrorPage from '../../Components/ErrorPage'
 import Navbar from '../../Components/NavBar'
+import SingleItemCard from '../../Components/SingleItemCard'
+import BoxWithButton from '../../Components/BoxWithButton'
+
 export function ShowFixedAsset() {
     const { fixedAssetId } = useParams()
-    const [url, setSomeUrl] = useState(`https://ncv-api.herokuapp.com/api/fixedAssets/${fixedAssetId}`)
+    const url = `https://ncv-api.herokuapp.com/api/fixedAssets/${fixedAssetId}`
     const { apiData:fixedAsset, error } = getFromApi(url)
-
+    let imageUrl = "https://comovertodogratis.com/wp-content/uploads/2021/05/1621943166_La-computadora-portatil-Teclast-F15-hoy-a-un-precio-increible-2048x1280.jpg" 
+        
     if(error){
         return ErrorPage(error)
     }
     if (!fixedAsset) return null
+    const fixedAssetData = { 
+        "NOMBRE": fixedAsset.name,
+        "DESCRIPCIÓN" : fixedAsset.description,
+        "FECHA DE ENTRADA": fixedAsset.entryDate!=null? fixedAsset.entryDate.split('T')[0]:null,
+        "PRECIO" : fixedAsset.price, 
+        "CARACTERÍSTICAS":fixedAsset.features,
+        "CANTIDAD" : fixedAsset.quantity
+    }
+    console.log(fixedAssetData)
     return (
-        <><Navbar /><div style={{ marginTop: '10vh' }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    ></IconButton>
-                </Toolbar>
-            </AppBar>
-            <div>
-                <ul>
-                    <Card style={{ minHeight: '80vh' }}>
-                        <li>
-                            <CardHeader title={fixedAsset.name} />
-                            <Box
-                                component="img"
-                                sx={{
-                                    height: 533,
-                                    width: 750,
-                                    maxHeight: { xs: 733, md: 567 },
-                                    maxWidth: { xs: 750, md: 750 }
-                                }}
-                                alt="Activo fijo."
-                                src="https://comovertodogratis.com/wp-content/uploads/2021/05/1621943166_La-computadora-portatil-Teclast-F15-hoy-a-un-precio-increible-2048x1280.jpg" />
-                            <CardContent>
-                                <h4>DESCRIPCIÓN: {fixedAsset.description}</h4>{' '}
-                                <br></br>
-                                <h4>
-                                    FECHA DE ENTRADA:{' '}
-                                    {fixedAsset.entryDate!=null? fixedAsset.entryDate.split('T')[0]:null}
-                                </h4>
-                                <br></br>
-                                <h4>PRECIO: {fixedAsset.price}</h4>
-                                <br></br>
-                                <h4>CARACTERÍSTICAS: {fixedAsset.features}</h4>
-                                <br></br>
-                                <h4>CANTIDAD: {fixedAsset.quantity}</h4>
-                                <br></br>
-                            </CardContent>
-                        </li>
-                    </Card>
-                </ul>
+        <>
+            <Navbar />
+            <div style={{ marginTop: '11vh' }}>
+                <SingleItemCard element={fixedAssetData} imageUrl={imageUrl} imageCirle={false} />        
             </div>
-        </div></>
-    )
+        </>
+        )
 }
