33c4746e5222437698d87c5e21c91117e6a288d2
miky14n <miky14n@gmail.com>
2022-10-05 17:24:19 -0400

    Correccion de la prueba de login

--- web-ui/cypress/e2e/Login.cy.js
+++ web-ui/cypress/e2e/Login.cy.js
@@ -1,5 +1,3 @@
-//Es necesario para las pruebas e2e instanciar esta variable debido que si no lo hacen se redirige a la vista por defecto que es el login
-sessionStorage.setItem('Role',"Soporte")
 
 describe('Login', () => {
   it('muestra la pagina de LogIn', () => {
