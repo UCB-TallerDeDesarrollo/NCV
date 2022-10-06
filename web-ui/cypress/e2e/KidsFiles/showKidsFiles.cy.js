f6c7e1f64158e342c22a87bcd41e6164765e0337
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-10-06 00:31:15 -0400

    test_e2e_show list kids

---
+++ web-ui/cypress/e2e/KidsFiles/showKidsFiles.cy.js
@@ -0,0 +1,14 @@
+describe('Show kids end to end tests', () => {
+  it('Shows the list of kids', () => {
+    cy.intercept('GET', 'https://ncv-api.herokuapp.com/api/kids',{
+      fixture: 'kids/listOfKids.json'
+    }).as('listOfKids',);
+
+    cy.visit('/ninos');
+
+    cy.get('.ListElement')
+    .and('contain', 'Pedrulas')
+    .should('have.length', 5)
+  });  
+});
+
