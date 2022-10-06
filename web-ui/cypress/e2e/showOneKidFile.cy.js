221c23071be05b0539c80634ca69d3cc78e40b8d
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-10-06 09:04:12 -0400

    Added basic e2e of viewKidFile

---
+++ web-ui/cypress/e2e/showOneKidFile.cy.js
@@ -0,0 +1,22 @@
+//Es necesario para las pruebas e2e instanciar esta variable debido que si no lo hacen se redirige a la vista por defecto que es el login
+sessionStorage.setItem('Role',"Soporte")
+
+describe('Show a file kid end to end tests', () => {
+    it('Verifies the fields from a single kid file', () => {
+      cy.intercept('GET', 'https://ncv-api.herokuapp.com/api/kids/42',{
+        fixture: '/aBasicInfo.json'
+      }).as('getBasicInfo',);
+  
+      cy.visit('/ninos/42');
+  
+      cy.get('div')
+      cy.contains('Elizabeth')
+      cy.contains('Ortega Lara')
+      cy.contains('79235642')
+      // cy.contains('12/9/2002') // it does not work
+      cy.contains('20')
+      cy.contains('SDE')
+      cy.contains('Cochabamba - Cercado')
+      cy.contains('F')
+    });
+  });
