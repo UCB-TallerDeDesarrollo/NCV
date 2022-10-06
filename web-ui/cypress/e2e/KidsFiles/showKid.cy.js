506c59c130815feaae650557286ee890a83f706c
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-10-06 02:36:37 -0400

    test_UTViews

--- web-ui/cypress/e2e/KidsFiles/showKid.cy.js
+++ web-ui/cypress/e2e/KidsFiles/showKid.cy.js
@@ -11,19 +11,6 @@ describe('Show a kid end to end tests', () => {
       cy.get('div')
       .should('have.length', 1)
     });
-    it('Verifies the fields from a single kid', () => {
-      cy.intercept('GET', 'https://ncv-api.herokuapp.com/api/kids/6',{
-        fixture: 'kids/anKid.json'
-      }).as('getAnKid',);
-  
-      cy.visit('/ninos/6');
-  
-      cy.get('div')
-      cy.contains('Joaquin')
-      cy.contains('Hernan Gomez')
-      cy.contains("8796235")
-      cy.contains('Av. Cualquier cosa #11')
-      cy.contains("España")
-    });
+    
   });
   
