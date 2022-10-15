8e4e355c8a3dbd66b0a14b901d8c9f761ffcef64
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-10-15 18:47:11 -0400

    ProgramHouse Entity added

---
+++ api/NinosConValorAPI/Data/Entity/ProgramHouseEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NinosConValorAPI.Data.Entity
+{
+    public class ProgramHouseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Acronym { get; set; }
+        public string? Location { get; set; }
+
+        [Required]
+        [ForeignKey("AspNetUsers")]
+        public IKey? ResponsibleId { get; set; }
+
+
+
+
+    }
+}
