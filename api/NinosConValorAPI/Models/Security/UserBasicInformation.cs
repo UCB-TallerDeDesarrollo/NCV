ab8ea86943f5cbddebe3b64f2e9ac35def81148f
miky14n <miky14n@gmail.com>
2022-10-19 15:48:24 -0400

    Se creo un modelo para poder guardar la informacion basica del usuario

---
+++ api/NinosConValorAPI/Models/Security/UserBasicInformation.cs
@@ -0,0 +1,11 @@
+﻿namespace NinosConValorAPI.Models.Security
+{
+    public class UserBasicInformation
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CellPhone { get; set; }
+        public string NameRole { get; set; }
+    }
+}
