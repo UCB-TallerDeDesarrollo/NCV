37312f9025e1af04e7eabd746d47f89b9ffe704a
Kevin <kevcespedesarancibia87@gmail.com>
2022-10-19 15:56:52 -0400

    BiometricsService done with UT

--- api/NinosConValorAPI/Services/BiometricsService.cs
+++ api/NinosConValorAPI/Services/BiometricsService.cs
@@ -1,6 +1,37 @@
-﻿namespace NinosConValorAPI.Services
+﻿using AutoMapper;
+using NinosConValorAPI.Data.Repository;
+using NinosConValorAPI.Models;
+
+namespace NinosConValorAPI.Services
 {
-    public class BiometricsService
+    public class BiometricsService : IBiometricsService
     {
+        private INCVRepository _appRepository;
+        private IMapper _mapper;
+        public BiometricsService(INCVRepository appRepository, IMapper mapper)
+        {
+            _appRepository = appRepository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<BiometricsModel>> GetBiometricsAsync(int kidId)
+        {
+            var biometricsList = await _appRepository.GetBiometricsAsync(kidId);
+            return _mapper.Map<IEnumerable<BiometricsModel>>(biometricsList);
+        }
+
+        public Task<BiometricsModel> CreateBiometricsAsync(int kidId, BiometricsModel biometrics)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteBiometricsAsync(int kidId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BiometricsModel> UpdateBiometricsAsync(int kidId, BiometricsModel biometrics)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
