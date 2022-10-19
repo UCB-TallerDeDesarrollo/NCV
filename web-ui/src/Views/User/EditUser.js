3856e607a3db1a7213283434baa649180fd1acf9
Andres <53712714+Andres-Pereira@users.noreply.github.com>
2022-10-19 18:57:05 -0400

    View for edit user
    This is the form that will be used to edit a user, still needs to obtain the user we want to edit from backend and call to the update function.

---
+++ web-ui/src/Views/User/EditUser.js
@@ -0,0 +1,166 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import Navbar from '../../Components/NavBar'
+import FormContainer from '../../Components/FormContainer'
+import InputText from '../../Components/InputText'
+import Collapse from '@mui/material/Collapse'
+import MenuItem from '@mui/material/MenuItem'
+import ButtonPrimary from '../../Components/MUI-Button'
+import Alert from '@mui/material/Alert'
+
+//Recuperar un usuario de backend de modo que los input text se llenen con los datos del usuario que seleccionamos
+
+const user = {
+    firstName: 'Oliver',
+    lastName: 'Martinez',
+    cellPhone: '67985357',
+    email: 'oliver@gmail.com',
+    password: 'Soporte!23',
+    confirmPassword: 'Soporte!23',
+    rol: 'AuntUser'
+}
+const roles = [
+    {
+        label: 'Tia',
+        value: 'AuntUser'
+    },
+    {
+        label: 'Administrador',
+        value: 'AdminUser'
+    }
+]
+
+function EditUser() {
+    var url = 'https://ncv-api.herokuapp.com/api/auth'
+    const navigate = useNavigate()
+    const [open, setOpen] = useState(false)
+    const [data, setData] = useState(user)
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+        setOpen(false)
+        setData({
+            ...data,
+            [name]: value
+        })
+    }
+
+    function handleFormSubmit() {
+        axios
+            .post(`${url}/${data.rol}`, data)
+            .then(function (response) {
+                if (response.status == 200) {
+                    navigate(`/inicio-ncv`, {
+                        state: {
+                            showAlert: true,
+                            alertMessage: 'Usuario editado exitosamente'
+                        }
+                    })
+                }
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    if (error.response.status == 400) setOpen(true)
+                }
+            })
+    }
+
+    return (
+        <>
+            <Navbar />
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: '3em'
+                }}
+            >
+                <FormContainer title="Editar datos de usuario">
+                    <Collapse in={open} sx={{ width: 1, pt: 2 }}>
+                        <Alert severity="error">
+                            Todos los campos son requeridos
+                        </Alert>
+                    </Collapse>
+
+                    <InputText
+                        required
+                        id="firstName"
+                        name="firstName"
+                        label="Nombre"
+                        type="text"
+                        value={data.firstName}
+                        onChange={handleInputChange}
+                    />
+                    <InputText
+                        required
+                        id="lastName"
+                        name="lastName"
+                        label="Apellido"
+                        type="text"
+                        value={data.lastName}
+                        onChange={handleInputChange}
+                    />
+                    <InputText
+                        required
+                        id="cellPhone"
+                        name="cellPhone"
+                        label="Celular"
+                        type="number"
+                        value={data.cellPhone}
+                        onChange={handleInputChange}
+                    />
+                    <InputText
+                        required
+                        id="email"
+                        name="email"
+                        label="Correo electronico"
+                        type="email"
+                        value={data.email}
+                        onChange={handleInputChange}
+                    />
+                    <InputText
+                        required
+                        id="password"
+                        name="password"
+                        label="Contraseña"
+                        type="password"
+                        value={data.password}
+                        onChange={handleInputChange}
+                    />
+                    <InputText
+                        required
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        label="Confirmar contraseña"
+                        type="password"
+                        value={data.confirmPassword}
+                        onChange={handleInputChange}
+                    />
+                    <InputText
+                        required
+                        select
+                        id="rol"
+                        name="rol"
+                        label="Rol"
+                        type="text"
+                        value={data.rol}
+                        onChange={handleInputChange}
+                    >
+                        {roles.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </InputText>
+                    <ButtonPrimary
+                        label={'Editar'}
+                        //onClick={handleFormSubmit}
+                    />
+                </FormContainer>
+            </div>
+        </>
+    )
+}
+
+export default EditUser
