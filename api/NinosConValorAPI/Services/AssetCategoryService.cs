dd80bdce615f589eba3f9d87af03146b6d5052e1
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-10-20 15:22:01 -0400

    Creating Asset Category Service

---
+++ api/NinosConValorAPI/Services/AssetCategoryService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using NinosConValorAPI.Data.Repository;
+using NinosConValorAPI.Models;
+
+namespace NinosConValorAPI.Services
+{
+    public class AssetCategoryService : IAssetCategoryService
+    {
+        private INCVRepository _NCVRepository;
+        private IMapper _mapper;
+        public AssetCategoryService(INCVRepository nCVRepository, IMapper mapper)
+        {
+            _NCVRepository = nCVRepository;
+            _mapper = mapper;
+        }
+
+        public Task<AssetCategoryModel> CreateAssetCategoryAsync(AssetCategoryModel assetCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AssetCategoryModel>> GetAssetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AssetCategoryModel> GetAssetCategoryAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
