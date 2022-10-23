4c1f34b8a12b667baf175f316b82d661d36834b4
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-10-23 13:11:41 -0400

    Editar fileKid

--- web-ui/src/Views/KidsFiles/ShowOneKidFile.js
+++ web-ui/src/Views/KidsFiles/ShowOneKidFile.js
@@ -146,6 +146,7 @@ function ShowOneKidFile() {
     return (
         <><Navbar /><div style={{ marginTop: '11vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center' }}>
             <SingleItemCard key={0} element={MyKidDetails} imageUrl={imageUrl} title={kid.firstName + " " + kid.lastName }/>
+            <ButtonPrimary key={2} label="EditarFile" onClick={()=>{navigate(`/ninos/${kidId}/editar-nino`)}} />
             <HealthReport kidId={kidId} healthReport={healthReport} healthReportStatusCode={healthReportStatusCode}/>
             <WeightAndHeight weightAndHeightData={biometrics}/>
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
