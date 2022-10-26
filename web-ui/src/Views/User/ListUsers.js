eb7a7f399b95bff98c5da605b60f10561b04c444
hmuyba <50967848+hmuyba@users.noreply.github.com>
2022-10-26 16:59:15 -0400

    standar  message of user created succesfully added

--- web-ui/src/Views/User/ListUsers.js
+++ web-ui/src/Views/User/ListUsers.js
@@ -1,18 +1,36 @@
 import ErrorPage from '../../Components/ErrorPage'
 import getFromApi from '../../Components/GetFromApi'
-
+import Alert from '@mui/material/Alert';
+import { Snackbar } from '@mui/material';
 import Box from '@mui/material/Box';
 import ListContainer from "../../Components/ListContainer";
 import Navbar from '../../Components/NavBar';
 import ButtonPrimary from '../../Components/MUI-Button';
 import ListBasic from '../../Components/ListBasic';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react'
 
 function ListUsers() {
     const url="https://ncv-api.herokuapp.com/api/auth";
     const { apiData:users, error } = getFromApi(url)
+
+    const location = useLocation()
+    
+    let showAlert = location.state ? location.state.showAlert : false 
+    let alertMessage = location.state ? location.state.alertMessage : null 
+    const [open, setOpen] = useState(showAlert);
+
+    function handleClose(event, reason) {
+        if (reason === 'clickaway') {
+            return
+        }
+        setOpen(false)
+    }
+
    // const location = useLocation()
     const completeInfoUser= "/vista-usuarios"
+ 
+
     const navigate = useNavigate()
     if(error){
         return ErrorPage(error)
@@ -38,6 +56,12 @@ function ListUsers() {
                         {usersComponents}
                     </ListContainer>
                 </Box>
+                
+                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success">
+                    {alertMessage}
+                </Alert>
+        </Snackbar>
             </>
         );
     }
