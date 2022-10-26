9ef82f2257c9c71f97c35e6256582c30dea65148
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-10-26 17:50:55 -0400

    hot fix

--- web-ui/src/Views/FixedAssets/ShowFixedAssets.js
+++ web-ui/src/Views/FixedAssets/ShowFixedAssets.js
@@ -3,15 +3,15 @@ import React, { useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
 import ErrorPage from '../../Components/ErrorPage'
 import {getFixedAssets} from '../../Components/GetFromApi'
+import getFromApi from '../../Components/GetFromApi'
 import Navbar from '../../Components/NavBar'
 import ListContainer from "../../Components/ListContainer"
 import ListBasic from '../../Components/ListBasic'
-import ButtonPrimary from '../../Components/MUI-Button';
+import ButtonPrimary from '../../Components/MUI-Button';    
 import Snackbar from '@mui/material/Snackbar'
 import Alert from '@mui/material/Alert'
 import SearchBar from '../../Components/SearchBar';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { List, ListSubheader } from '@mui/material'
 
 export default function ShowFixedAssets() {
     const [open, setOpen] = useState(null);
@@ -24,13 +24,11 @@ export default function ShowFixedAssets() {
     const navigate = useNavigate();
 
     const completeInfoFixedAsset = '/activos-fijos'
-    const url = 'https://ncv-api.herokuapp.com/api/fixedAssets'
-    const urlCategories = 'https://ncv-api.herokuapp.com/api/assetCategories'
+    const url = 'https://ncv-api.herokuapp.com/api/fixedAssets/'
+    const urlCategories = 'https://ncv-api.herokuapp.com/api/assetCategories?showAssets=true'
     let showAlert = location.state ? location.state.showAlert : false
     let alertMessage = location.state ? location.state.alertMessage : null
-    const [open, setOpen] = useState(showAlert);
-    const { apiData: fixedAssetsList, error } = getFromApi(url)
-    const { apiData: assetCategories, errors2 } = getFromApi(urlCategories)
+    const { apiData: assetCategories, errors } = getFromApi(urlCategories)
     const headerIndices = [];
     const getHeaderName = (i) => {
         switch (i) {
@@ -68,60 +66,46 @@ export default function ShowFixedAssets() {
         )
         setOpen(showAlert)
     },[])
+
     function handleClose(event, reason) {
         if (reason === 'clickaway') {
             return
         }
         setOpen(false)
     }
-    if (error) {
-        return ErrorPage(error)
-    }
-    if (assetCategories) {
-        assetCategories.map((cat) => {
-            headerIndices.push(cat.category); //agregar los indices a la lista 
-        })
-    }
-    if (!fixedAssets) return null
+
     if (hasErrorWithFetch != null){
-        console.log(typeof(hasErrorWithFetch.response.data))
         return ErrorPage(hasErrorWithFetch)
     } 
-    if (!fixedAssets) return null
+    if (!fixedAssets || !assetCategories) return null
     const searcher = <SearchBar posts={fixedAssets} setSearchResults={setSearchResults} orderCriteria={ordenCriteria} searchCriteria={searchCriteria} />
-    
-    if (fixedAssets.length>0){
-        const listElements = searchResult.map((el)=>{
+    if (fixedAssets.length>0 && assetCategories.length>0){
+        const listCategories = assetCategories.map((el)=>{
             return {
-                id: el.id,
-                title: `${el.name}`,
-                description: `Programa: ${el.programHouseAcronym != null && el.programHouseAcronym != "" && el.programHouseAcronym != undefined ? el.programHouseAcronym : "*Sin programa*"}`,
-                elementUrl: `${completeInfoFixedAsset}/${el.id}`,
-                imgSrc: `https://st.depositphotos.com/1005574/2080/v/450/depositphotos_20808761-stock-illustration-laptop.jpg`,
-                categoryName: el.assetCategoryCategory
+                id:el.id, 
+                title:`${el.category}`,
+                description:``,
             }
         })
-        const groups = listElements.reduce((groups, item) => {
-            const group = (groups[item.categoryName] || []);
-            group.push(item);
-            groups[item.categoryName] = group;
-            return groups;
-        }, {});
-        function getFixedAssetsComponent(category){
-            return <ListBasic items={groups[category]} withImage={false} />
-        }
+        const listElements = searchResult.map((el)=>{
+            return {
+                id:el.id, 
+                title:`${el.name}`,
+                description:`Programa: ${el.programHouseAcronym!=null&&el.programHouseAcronym!=""&&el.programHouseAcronym!=undefined?el.programHouseAcronym:"*Sin programa*"}`,                 
+                elementUrl:`${completeInfoFixedAsset}/${el.id}`,
+                imgSrc:`https://st.depositphotos.com/1005574/2080/v/450/depositphotos_20808761-stock-illustration-laptop.jpg`                
+            }
+        })
+        let assetCategoriesComponent = <ListBasic items={listCategories} withImage={false} />
         let fixedAssetsComponent = <ListBasic items={listElements} withImage={false} />
-        let fixedAssetsComponentCat1 = <ListBasic items={groups["Equipos y Herramientas"]} withImage={false} />
-        let fixedAssetsComponentCat2 = <ListBasic items={groups["Muebles y Enseres"]} withImage={false} />
-        let fixedAssetsComponentCat3 = <ListBasic items={groups["Maquinaria"]} withImage={false} />
-        let fixedAssetsComponentCat4 = <ListBasic items={groups["Herramientas"]} withImage={false} />
         let nexFixedAsset = "/crear-activo-fijo"
-        const listHeaderComponents = <ButtonPrimary label={"Crear activo fijo"} onClick={() => navigate(nexFixedAsset)} />
+        const listHeaderComponents = <ButtonPrimary label={"Crear activo fijo"} onClick={()=>navigate(nexFixedAsset)}/>
         return (
             <>
-                <Navbar /><Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '15vh' }}>
+                <Navbar /><Box sx={{ display: 'flex', justifyContent: 'center' , marginTop:'15vh'}}>
                     <ListContainer title="Lista de activos fijos" header={listHeaderComponents}>
                         {searcher}
+                        {assetCategoriesComponent}
                         {fixedAssetsComponent}
                     </ListContainer>
                 </Box>
