fd68fad4a6d30c9f7ac73bc454b115f0e48a632a
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2022-10-26 16:07:28 -0400

    Fixing a problem with ErrorPage

--- web-ui/src/Components/ErrorPage.jsx
+++ web-ui/src/Components/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 function ErrorPage (error) {
-    const message = error.response.data != ""? error.response.data : error.response.statusText 
+    const message = typeof(error.response.data) == "string" && error.response.data.lenght > 0 ? error.response.data : error.response.statusText 
     const httpStatus = error.response.status
     return <h1>ERROR {httpStatus}: {message}</h1>
 }
