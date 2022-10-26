8f8eb672cbf538bc19421a69abb6d83658974a25
OmarArias <omarxarias@gmail.com>
2022-10-26 16:11:48 -0400

    Prueba sin probar

--- web-ui/cypress/e2e/fixedAssets/showKidsFiles.cy.js
+++ web-ui/cypress/e2e/fixedAssets/showKidsFiles.cy.js
@@ -14,3 +14,19 @@ describe('Show kids end to end tests', () => {
   });  
 });
 
+describe('Show searched kids end to end tests', () => {
+  it('Shows the list of kids', () => {
+    cy.intercept('GET', 'https://ncv-api.herokuapp.com/api/kids',{
+      fixture: 'Kids/listOfKids.json'
+    }).as('listOfKids',);
+
+    cy.visit('/ninos');
+
+    cy.get('.SearchBar').type('Pe');
+
+    cy.get('.ListElement')
+    .and('contain', 'Pedrulas')
+    .should('have.length', 5)
+  });  
+});
+
