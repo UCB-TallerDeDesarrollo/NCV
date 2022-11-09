54fac43cb3be2244b52ec36f0fcb6ae7c6c8abf2
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-11-08 23:32:42 -0400

    Model EducationReport

--- api/NinosConValorAPI/Models/EducationReportModel.cs
+++ api/NinosConValorAPI/Models/EducationReportModel.cs
@@ -7,7 +7,6 @@ namespace NinosConValorAPI.Models
         public int Id { get; set; }
         public int KidId { get; set; }
         public string? Grade { get; set; }
-        [Required]
         public string? School { get; set; }
         public string? Rude { get; set; }
     }
