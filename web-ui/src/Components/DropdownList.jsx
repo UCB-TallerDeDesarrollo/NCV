5bebf420db7f77d8ddf9df00a7d7b7689805559b
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-11-12 18:59:15 -0400

    Mostrando/Ocultando Activos Fijos por una o varias Categorías seleccionadas

--- web-ui/src/Components/DropdownList.jsx
+++ web-ui/src/Components/DropdownList.jsx
@@ -4,12 +4,9 @@ import { ListItemButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { useState } from "react";
 
-export default function ListWithSubheader({itemsHeader, itemsSubheader, withImage=true}) {
-  const [isVisible, setIsVisible] = useState(false);
+export default function DropdownList({itemsHeader, itemsSubheader}) {
+  const [isVisible, setIsVisible] = useState({});
   const navigate = useNavigate();
-  const handlePress = () => {
-    setIsVisible(isThisVisible => !isThisVisible);
-  };
   const sxListItemText = {
     '& .MuiListItemText-primary': {
       fontSize: 18,
@@ -20,10 +17,15 @@ export default function ListWithSubheader({itemsHeader, itemsSubheader, withImag
     <List sx={{ width: '100%', bgcolor: 'background.paper', alignItems :"flex-start" }}>
       {itemsHeader.map((h,i)=>{
         return (<>
-            <ListItemButton sx={{borderTop: 1, borderColor:'#CDCDCD', margin:0}} key={h.id} alignItems="flex-start" onClick={() => handlePress()}>
-              <ListItemText primary={h.title} secondary={h.description} className="ListElement" sx={sxListItemText}/>
+            <ListItemButton sx={{borderTop: 1, borderColor:'#CDCDCD', margin:0}} key={h.id} alignItems="flex-start"
+              onClick={() => setIsVisible({
+                ...isVisible,
+                [h.title]: !isVisible?.[h.title],
+              })}
+            >
+            <ListItemText primary={h.title} secondary={h.description} className="ListElement" sx={sxListItemText}/>
             </ListItemButton>
-                {isVisible && itemsSubheader.map((s,i)=>{
+                {!isVisible?.[h.title] ? null : itemsSubheader.map((s,i)=>{
                         if (h.id == s.categoryId) {
                             return (
                               <ListItemButton sx={{borderTop: 1, borderColor:'#CDCDCD', m:0, pl: 3, pr: 3}} key={s.id} alignItems="flex-start" onClick={()=>navigate(s.elementUrl)}>
