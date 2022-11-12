7333e1e5c80e962d0245949e0db6bead38ce73d6
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-11-11 21:07:14 -0400

    deleted field added to assetState

---
+++ api/NinosConValorAPI/Migrations/20221112010619_assetStatusDeletedField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NinosConValorAPI.Migrations
+{
+    public partial class assetStatusDeletedField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "AssetState",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "AssetState");
+        }
+    }
+}
