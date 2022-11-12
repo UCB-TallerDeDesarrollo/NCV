1f60b650b6b70bdf385d05cd2102924ead1a3a7b
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-11-12 18:10:13 -0400

    Mostrando/Ocultando Activos Fijos por categorías

--- web-ui/src/Components/ListWithSubheader.jsx
+++ web-ui/src/Components/ListWithSubheader.jsx
@@ -1,12 +1,15 @@
 import List from '@mui/material/List';
-import ListElement from './ListElement'
-import { ListSubheader } from '@mui/material';
 import ListItemText from '@mui/material/ListItemText';
 import { ListItemButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
 export default function ListWithSubheader({itemsHeader, itemsSubheader, withImage=true}) {
+  const [isVisible, setIsVisible] = useState(false);
   const navigate = useNavigate();
+  const handlePress = () => {
+    setIsVisible(isThisVisible => !isThisVisible);
+  };
   const sxListItemText = {
     '& .MuiListItemText-primary': {
       fontSize: 18,
@@ -17,10 +20,10 @@ export default function ListWithSubheader({itemsHeader, itemsSubheader, withImag
     <List sx={{ width: '100%', bgcolor: 'background.paper', alignItems :"flex-start" }}>
       {itemsHeader.map((h,i)=>{
         return (<>
-            <ListItemButton sx={{borderTop: 1, borderColor:'#CDCDCD', margin:0}} key={h.id} alignItems="flex-start" onClick={()=>navigate(h.elementUrl)}>
+            <ListItemButton sx={{borderTop: 1, borderColor:'#CDCDCD', margin:0}} key={h.id} alignItems="flex-start" onClick={() => handlePress()}>
               <ListItemText primary={h.title} secondary={h.description} className="ListElement" sx={sxListItemText}/>
             </ListItemButton>
-                {itemsSubheader.map((s,i)=>{
+                {isVisible && itemsSubheader.map((s,i)=>{
                         if (h.id == s.categoryId) {
                             return (
                               <ListItemButton sx={{borderTop: 1, borderColor:'#CDCDCD', m:0, pl: 3, pr: 3}} key={s.id} alignItems="flex-start" onClick={()=>navigate(s.elementUrl)}>
