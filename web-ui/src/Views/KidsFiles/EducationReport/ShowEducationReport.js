8c1492eb01364c7c965fa42fbaecd25085669168
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-11-16 22:31:32 -0400

    show EducationReport

---
+++ web-ui/src/Views/KidsFiles/EducationReport/ShowEducationReport.js
@@ -0,0 +1,37 @@
+import { useNavigate } from 'react-router-dom';
+import Container from '../../../Components/Container';
+import Box from '@mui/material/Box';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import { Typography } from '@mui/material';
+
+import SingleItemCard from '../../../Components/SingleItemCard'
+import ButtonPrimary, { ButtonDanger, ButtonSecondary } from '../../../Components/MUI-Button';
+
+function EducationReport({kidId, educationReport, educationReportStatusCode}){
+    const navigate = useNavigate();
+    let urlCreateEducationReport = `/ninos/${kidId}/crear-reporte-education/`
+    let buttonCreateEducationReport = (<Container sx={{ p: 0 , pt: 0, m:0, width:1, borderRadius:0, border:0, boxShadow:0}}>
+        <Box sx={{display:"flex", flexDirection:"column", justifyContent: 'center', alignItems: 'center'}}>
+            <AutoAwesomeIcon sx={{marginTop:2}}/>
+            <Box sx={{margin:3}}>
+                <Typography variant="body2">No se registraron datos para el <b>reporte de Educacion</b></Typography>
+            </Box>
+            <ButtonPrimary key={2} label="Crear reporte Educación" onClick={()=>{navigate(urlCreateEducationReport)}} />
+        </Box>
+    </Container>);
+    let educationReportComponent = null
+    if (educationReportStatusCode == 404){
+        educationReportComponent = buttonCreateEducationReport
+    }
+    if (educationReport != null && educationReportStatusCode == 200){
+        var educationReportElement = {
+            "Grade" : educationReport.Grade,
+            "School" : educationReport.School ,
+            "Rude" : legalReport.Rude,
+        }
+        educationReportComponent = <SingleItemCard key={1} element={educationReportElement} title={"Reporte Educación"} sx={{ p: 0 , pt: 0, m:0, width:1, borderRadius:0, border:0, boxShadow:0}}/>
+    }
+    return educationReportComponent
+}
+
+export default EducationReport;
