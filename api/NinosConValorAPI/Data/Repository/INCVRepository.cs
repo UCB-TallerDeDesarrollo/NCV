a296f36c6fcaf67afbdc8f72f2e47a9bd8fecaab
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-11-24 10:00:40 -0400

    Repository Legal Report

--- api/NinosConValorAPI/Data/Repository/INCVRepository.cs
+++ api/NinosConValorAPI/Data/Repository/INCVRepository.cs
@@ -38,6 +38,7 @@ namespace NinosConValorAPI.Data.Repository
         // CONTACTS
         Task<IEnumerable<ContactEntity>> GetContactsAsync(int kidId);
         Task<ContactEntity> CreateContactAsync(ContactEntity contact);
+        Task<ContactEntity> UpdateContactAsync(int kidId, ContactEntity contact);
 
         //PROGRAM HOUSE
         Task<IEnumerable<ProgramHouseEntity>> GetProgramHousesAsync();
@@ -79,6 +80,7 @@ namespace NinosConValorAPI.Data.Repository
         // EDUCATION REPORT
         public Task<FamilyReportEntity> CreateFamilyReportAsync(FamilyReportEntity familyReport);
         Task<FamilyReportEntity> GetFamilyReportAsync(int kidId);
+        Task<EducationReportEntity> UpdateEducationReportAsync(int kidId, EducationReportEntity educationReport);
 
         // LEGAL REPORT
         public Task<LegalReportEntity> CreateLegalReportAsync(LegalReportEntity legalReport);
