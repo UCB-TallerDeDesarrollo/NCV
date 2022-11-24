cefbae194f2509eab62a28ac65ce9ead564c612f
miky14n <miky14n@gmail.com>
2022-11-24 14:01:36 -0400

    Ultimos cambios

--- web-ui/src/Components/GutterList.jsx
+++ web-ui/src/Components/GutterList.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from 'react-router-dom';
 import EditIcon from '@mui/icons-material/Edit';
 import ButtonPrimary, { ButtonDanger, ButtonSecondary } from './MUI-Button';
 import Alert from '@mui/material/Alert';
-import { Snackbar } from '@mui/material';
+import { ListItemIcon, Snackbar } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -37,34 +37,37 @@ export default function GutterlessList({items, withImage=true, withDeleteIcon=fa
         <List sx={{ width: '100%', bgcolor: 'background.paper', alignItems :"flex-start" }}>
         {items.map((n,i)=> (
             <ListItem
-                key={n.id ? n.id : i} id={n.id} title={n.title} description={n.description} elementUrl={n.elementUrl} imgSrc={n.imgSrc} 
+                key={n.id ? n.id : i} id={n.id}  description={n.description} elementUrl={n.elementUrl} 
                 
                 secondaryAction={
-                    <IconButton >
-                        <EditIcon fontSize="small" aria-label="edit" onClick={()=>navigate(n.elementUrl)} />
-                        {true &&<DeleteIcon fontSize="small" aria-label="delete" onClick={ToConfirmOpen} color='error' id={n.id} setSelectedRow={n}/>}
-                        
-                        
-                    </IconButton>
+                    <ListItemIcon>
+                        <IconButton title="Editar">
+                            <EditIcon fontSize="small" aria-label="Editar" onClick={()=>navigate(n.elementUrl)}  />                      
+                        </IconButton>
+                        <IconButton title="Eliminar">
+                            {true &&<DeleteIcon fontSize="small" aria-label="Eliminar" onClick={ToConfirmOpen} color='error' id={"Eliminar"} setSelectedRow={n}/>}
+                        </IconButton>
+                    </ListItemIcon>
                 }
+                
                 
             >
             <ListItemText primary={n.title} secondary={n.description} key={n.id ? n.id : i} id={n.id} sx={{borderTop: 1, borderColor:'#CDCDCD', margin:0}}/>
                 
             </ListItem>
         ))}
-        <Dialog open={openToConfirmDelete} onClose={handleCloseToConfirm} id="confirmation_popup" sx={{borderRadius:3 }}>
-                    <DialogTitle sx={{display:'flex', justifyContent:'center'}}>Eliminar</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText id="alert-dialog-description">
-                            ¿Desea eliminar todos los datos de {selectedRow.title}?
-                        </DialogContentText>
-                    </DialogContent>
-                    <DialogActions sx={{display:'flex',flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
-                        <ButtonSecondary label="Cancelar" onClick={handleCloseToConfirm}></ButtonSecondary>
-                        <ButtonDanger label="Eliminar" id="confirm_delete_button" onClick={fetchDeleteUSer}></ButtonDanger>
-                    </DialogActions>
-                </Dialog>
+         <Dialog open={openToConfirmDelete} onClose={handleCloseToConfirm} id="confirmation_popup" sx={{borderRadius:3 }}>
+            <DialogTitle sx={{display:'flex', justifyContent:'center'}}>Eliminar</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    ¿Desea eliminar todos los datos de {selectedRow.title}?
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions sx={{display:'flex',flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+                <ButtonSecondary label="Cancelar" onClick={handleCloseToConfirm}></ButtonSecondary>
+                <ButtonDanger label="Eliminar" id="confirm_delete_button" onClick={fetchDeleteUSer}></ButtonDanger>
+            </DialogActions>
+        </Dialog>
         </List>
         
     );
