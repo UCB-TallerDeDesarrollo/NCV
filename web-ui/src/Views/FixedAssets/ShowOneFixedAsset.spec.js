463d62d2f995d6e85e86a43e4107892bcd3f04cf
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-11-25 22:29:06 -0400

    Cambiando pruebas de unidad en mostrar un activo fijo

--- web-ui/src/Views/FixedAssets/ShowOneFixedAsset.spec.js
+++ web-ui/src/Views/FixedAssets/ShowOneFixedAsset.spec.js
@@ -23,7 +23,7 @@ describe('Show Fixed Asset', () => {
       id: 1,
       name: "Teclado",
       description: "Es un teclado razer",
-      entryDate: "2022-09-30T00:00:00",
+      entryDate: "2022-03-03T00:00:00",
       price: 200,
       features: "Color negro a medio uso",
       assetStateState: "Obsoleto"
@@ -67,7 +67,7 @@ describe('Show Fixed Asset', () => {
     await waitFor(() => {
         expect(screen.getByText('Teclado')).toBeVisible
         expect(screen.getByText('Es un teclado razer')).toBeVisible
-        expect(screen.getByText('29/9/2022')).toBeVisible
+        expect(screen.getByText('3/3/2022')).toBeVisible
         expect(screen.getByText('200')).toBeVisible
         expect(screen.getByText('Color negro a medio uso')).toBeVisible
         expect(screen.getByText('Obsoleto')).toBeVisible
