707992323fadf2391737d802564a6d5495c67844
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-11-27 19:00:08 -0400

    show assetTypes by categories

--- web-ui/src/Views/FixedAssets/AssetTypes/ShowAssetTypesByCategory.js
+++ web-ui/src/Views/FixedAssets/AssetTypes/ShowAssetTypesByCategory.js
@@ -17,55 +17,66 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import axios from "axios"
 import FormContainer from '../../../Components/FormContainer'
 import InputText from '../../../Components/InputText'
+import DropdownList from '../../../Components/DropdownList'
 
 var accesPermiss = sessionStorage.getItem("Access")
 
-export default function ShowFixedAssets() {
+export default function ShowAssetTypesByCategory() {
+    console.log("in")
     const navigate = useNavigate();
     const location = useLocation()    
     const [showAlert, setShowAlert] = useState(location.state ? location.state.showAlert : false)
     const [alertMessage, setAlertMessage] = useState(location.state ? location.state.alertMessage : null)
     const [severity, setSeverity] = useState(location.state ? location.state.severity : "success")
-    const urlAssetStates = 'https://ncv-api.azurewebsites.net/api/assetStates'
-    let [urlAssetState, setUrlAssetState] = useState('https://ncv-api.azurewebsites.net/api/assetStates/')   
-    const [assetStates, setAssetStates] = useState(null)
-    const [errorAssetStates, setErrorAssetStates] = useState(null)
+    //const urlAssetCategories = 'https://ncv-api.azurewebsites.net/api/assetCategories'
+    const urlAssetCategories = 'http://localhost:5009/api/AssetCategories/'
+    const [assetTypes, setAssetTypes] = useState(null)
+    const [assetCategories, setAssetCategories] = useState(null)
+    const [assetCategoryId, setAssetcategoryId] = useState(null)
+    const [errorAssetTypes, setErrorAssetTypes] = useState(null)
     let errorsFromForm = null
-    const [assetState, setAssetState] = useState([]) 
+    const [assetType, setAssetType] = useState([]) 
     const [open, setOpen] = useState(showAlert)
-    const [assetStateId, setAssetStateId] = useState(0)
+    const [assetTypeId, setAssetTypeId] = useState(0)
     const [openToConfirm, setOpenToConfirm] = useState(false)
-    const [errorAssetStateDelete, setErrorAssetStateDelete] = useState(null)
-    const [errorCreateAssetState, setErrorCreateAssetState] = useState(null)
-    const [errorUpdateAssetState, setErrorUpdateAssetState] = useState(null)
+    const [errorAssetTypeDelete, setErrorAssetTypeDelete] = useState(null)
+    const [errorCreateAssetType, setErrorCreateAssetType] = useState(null)
+    const [errorUpdateAssetType, setErrorUpdateAssetType] = useState(null)
     
     const [data, setData] = useState({
-        state:''//string
+        type:''//string
     })
 
     const [formErrors, setFormErrors] = useState({})
-    let assetStatesComponent = null   
 
-    function getAssetStates(){
-        axios.get(urlAssetStates).then(            
+    function listAssetTypes(assetTypesByCategory){
+        console.log("assetTypesByCategory= ",assetTypesByCategory)
+        let assetTypesByCats = assetTypesByCategory.map(actypes => 
+            actypes.assetTypes
+            )
+        setAssetTypes(assetTypesByCats.flat())
+        console.log("assetTypesByCats",assetTypesByCats.flat())       
+    }
+
+    function getAssetTypesByCategory(){
+        console.log("url: ",urlAssetCategories)
+        axios.get(urlAssetCategories).then(            
             (res) => {
-                setAssetStates(res.data)
+                listAssetTypes(res.data)
+                setAssetCategories(res.data)
             }
         ).catch((e)=>{
-            setErrorAssetStates(e)
+            setErrorAssetTypes(e)
         })
     }
 
     useEffect(() => {
-        axios.get(urlAssetStates).then(
-            res => setAssetStates(res.data)
-        ).catch((e)=>{
-            setErrorAssetStates(e)
-        })
+        console.log("use Effect")
+        getAssetTypesByCategory()
     }, [])
     
-    const fetchDeleteAssetState = () => {    
-        axios.delete(urlAssetState + assetStateId)
+    const fetchDeleteAssetType = () => {    
+        axios.delete(`${urlAssetCategories}${assetCategoryId}/assetTypes/${assetTypeId}`)
         .then(function (response) {
             if (response.status == 200){
                 setShowAlert(true)
@@ -73,11 +84,11 @@ export default function ShowFixedAssets() {
                 setSeverity("success")
                 setOpen(true)
                 setOpenToConfirm(false) 
-                getAssetStates()                                          
+                getAssetTypesByCategory()                                          
             }
         })
         .catch(err=> {
-            setErrorAssetStateDelete(err)     
+            setErrorAssetTypeDelete(err)     
             setOpenToConfirm(false)        
         })
     }
@@ -92,10 +103,10 @@ export default function ShowFixedAssets() {
 
     const validate = (datas) => {      
         const errors = {
-            state: '' // string            
+            type: '' // string            
         }        
-        if(!datas.state||datas.state.length==0)
-            errors.state= "El Estado es requerido!"
+        if(!datas.type||datas.type.length==0)
+            errors.type= "El tipo es requerido!"
         return errors     
     }
 
@@ -105,7 +116,7 @@ export default function ShowFixedAssets() {
         }      
         else{
             let updateData = {
-                state:value
+                type:value
             }
             submitUpdate(id,updateData)
         }  
@@ -113,16 +124,16 @@ export default function ShowFixedAssets() {
     }
 
     function submitUpdate(id,updateData){
-        axios.put(urlAssetState + id, updateData).then((res) => {
+        axios.put(`${urlAssetCategories}${assetCategoryId}/assetTypes/${id}`, updateData).then((res) => {
             if (res.status == 200) {               
                 setShowAlert(true)
-                setAlertMessage("Estado actualizado")
+                setAlertMessage("Tipo actualizado")
                 setSeverity("success")
                 setOpen(true)                    
-                getAssetStates()                    
+                getAssetTypesByCategory()                    
             }            
         }).catch ((apiError) => {
-            setErrorUpdateAssetState(apiError)                    
+            setErrorUpdateAssetType(apiError)                    
         })
     }
 
@@ -130,48 +141,51 @@ export default function ShowFixedAssets() {
         errorsFromForm = validate(data)
         setFormErrors(errorsFromForm)
         if(!hasFormErrors(errorsFromForm)){
-            axios.post(urlAssetState, data).then((res) => {
+            axios.post(`${urlAssetCategories}${assetCategoryId}/assetTypes/`, data).then((res) => {
                 if (res.status == 201) {     
                     setShowAlert(true)
-                    setAlertMessage("Estado creado")
+                    setAlertMessage("Tipo creado")
                     setSeverity("success")
                     setOpen(true)
-                    getAssetStates()
+                    getAssetTypesByCategory()
                     setData({
-                        state:''//string
+                        type:''//string
                     })
                 }            
             }).catch ((apiError) => {
-                setErrorCreateAssetState(apiError) 
+                setErrorCreateAssetType(apiError) 
                 checkError()                    
             })
         }else{
             console.log(errorsFromForm)
         }
     }
-    if (errorAssetStates) return ErrorPage(errorAssetStates)
-    if (errorAssetStateDelete){
-        if(errorAssetStateDelete.response.status==400 && errorAssetStateDelete.response.data=="El estado no puede ser eliminado porque existen activos fijos asociados a el."){
+    if (errorAssetTypes) return ErrorPage(errorAssetTypes)
+    if (errorAssetTypeDelete){
+        if(errorAssetTypeDelete.response.status==400 && errorAssetTypeDelete.response.data=="El tipo no puede ser eliminado porque existen activos fijos asociados a el."){
             setShowAlert(true)
-            setAlertMessage(errorAssetStateDelete.response.data)
+            setAlertMessage(errorAssetTypeDelete.response.data)
             setSeverity("warning")
             setOpen(true)
-            setErrorAssetStateDelete(null)
+            setErrorAssetTypeDelete(null)
         }
         else
-            return ErrorPage(errorAssetStateDelete)
+            return ErrorPage(errorAssetTypeDelete)
     } 
-    if (errorCreateAssetState) return ErrorPage(errorCreateAssetState)
-    if (errorUpdateAssetState) return ErrorPage(errorUpdateAssetState)
-    if (!assetStates) return null    
-    if (!assetState)return <h1>ERROR: Estado de activo fijo no encontrado en la base de datos</h1>
-    const assetStatesListElements = assetStates.map((assetState)=>{
+    if (errorCreateAssetType) return ErrorPage(errorCreateAssetType)
+    if (errorUpdateAssetType) return ErrorPage(errorUpdateAssetType)
+    if (!assetTypes) return null    
+    if (!assetType)return <h1>ERROR: Tipo de activo fijo no encontrado en la base de datos</h1>
+    
+    const assetTypesListElements = assetTypes.map((assetType)=>{
         return {
-            id:assetState.id, 
-            title: assetState.state,     
-            description: ''       
+            id:assetType.id, 
+            title: assetType.type,     
+            description: '',
+            categoryId: assetType.assetCategoryId    
         }
     })
+    console.log("assetTypesListElements = ", assetTypesListElements)
 
     function handleClose(event, reason) {
         if (reason === 'clickaway') {            
@@ -192,7 +206,7 @@ export default function ShowFixedAssets() {
     }
 
     let deleteAction = (id) => {
-        setAssetStateId(id)          
+        setAssetTypeId(id)          
         handleCloseToConfirm()
         ToConfirmOpen()
     }         
@@ -203,14 +217,21 @@ export default function ShowFixedAssets() {
         setData(newData)
         setOpen(false)
     }
-    
-    assetStatesComponent = <ListGrid items={assetStatesListElements} withImage={false} editable={true} editActionOnSave={handleSave} withDeleteIcon={true} deleteAction={deleteAction}/>
+    const listCategories = assetCategories.map((el)=>{
+        return {
+            id:el.id, 
+            title:`${el.category}`,
+            description:``
+        }
+    })
+    let assetCategoriesComponent = <DropdownList itemsHeader={listCategories} itemsSubheader={assetTypesListElements} isOpened={true} />
+    //assetTypesComponent = <ListGrid items={assetTypesListElements} withImage={false} editable={true} editActionOnSave={handleSave} withDeleteIcon={true} deleteAction={deleteAction}/>
     return (        
         <>        
             <Navbar /><Box sx={{ display: 'flex', justifyContent: 'center' , marginTop:'15vh'}}>
             {accesPermiss=="CompleteAccess"&&
-                <ListContainer title="Lista de Estados de Activos Fijos">
-                    {assetStatesComponent}
+                <ListContainer title="Lista de Tipos de Activos Fijos por Categoría">
+                    {assetCategoriesComponent}
                 </ListContainer>
             }   
             </Box>
@@ -223,31 +244,31 @@ export default function ShowFixedAssets() {
                 <DialogTitle sx={{display:'flex', justifyContent:'center'}}>Eliminar</DialogTitle>
                 <DialogContent>
                     <DialogContentText id="alert-dialog-description">
-                        ¿Desea eliminar el estado?
+                        ¿Desea eliminar el tipo?
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions sx={{display:'flex',flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
                     <ButtonSecondary label="Cancelar" onClick={handleCloseToConfirm}></ButtonSecondary>
-                    <ButtonDanger label="Eliminar" id="confirm_delete_button" onClick={fetchDeleteAssetState}></ButtonDanger>
+                    <ButtonDanger label="Eliminar" id="confirm_delete_button" onClick={fetchDeleteAssetType}></ButtonDanger>
                 </DialogActions>
             </Dialog>
             <div style={{display:'flex', justifyContent:'center'}}>
-        <FormContainer title="Crear estado">
+        <FormContainer title="Crear tipo">
             <InputText
                 required
-                id="state"
-                name="state"
-                value={data.state}
-                label="Estado"
+                id="type"
+                name="type"
+                value={data.type}
+                label="Tipo"
                 type="text"
                 onChange={(e) => {
                     handle(e)            
                 }}
             />
-            {formErrors.state? <Alert  sx={{ wieditdth: 1, pt: 1 }} severity="error"> 
-                {formErrors.state}                   
+            {formErrors.type? <Alert  sx={{ wieditdth: 1, pt: 1 }} severity="error"> 
+                {formErrors.type}                   
             </Alert>:<p></p> }
-            <ButtonPrimary label={"Crear estado"} id="submit_button" onClick={submitCreate}/>
+            <ButtonPrimary label={"Crear tipo"} id="submit_button" onClick={submitCreate}/>
             </FormContainer>
             </div>            
         </>                
