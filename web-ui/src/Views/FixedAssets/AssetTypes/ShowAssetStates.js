b536eb20be34755ff937f1d97c2eda3b957d17ed
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-11-27 17:17:57 -0400

    including types in assetCategories endpoint

---
+++ web-ui/src/Views/FixedAssets/AssetTypes/ShowAssetStates.js
@@ -0,0 +1,255 @@
+import React, { useEffect, useState } from 'react'
+import Box from '@mui/material/Box'
+import ErrorPage from '../../../Components/ErrorPage'
+import getFromApi from '../../../Components/GetFromApi'
+import Navbar from '../../../Components/NavBar'
+import ListContainer from '../../../Components/ListContainer'
+import ButtonPrimary, { ButtonDanger, ButtonSecondary } from '../../../Components/MUI-Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
+import DialogContentText from '@mui/material/DialogContentText'
+import Alert from '@mui/material/Alert'
+import { Snackbar } from '@mui/material'
+import ListGrid from '../../../Components/ListGrid'
+import { useNavigate, useLocation } from 'react-router-dom'
+import axios from "axios"
+import FormContainer from '../../../Components/FormContainer'
+import InputText from '../../../Components/InputText'
+
+var accesPermiss = sessionStorage.getItem("Access")
+
+export default function ShowFixedAssets() {
+    const navigate = useNavigate();
+    const location = useLocation()    
+    const [showAlert, setShowAlert] = useState(location.state ? location.state.showAlert : false)
+    const [alertMessage, setAlertMessage] = useState(location.state ? location.state.alertMessage : null)
+    const [severity, setSeverity] = useState(location.state ? location.state.severity : "success")
+    const urlAssetStates = 'https://ncv-api.azurewebsites.net/api/assetStates'
+    let [urlAssetState, setUrlAssetState] = useState('https://ncv-api.azurewebsites.net/api/assetStates/')   
+    const [assetStates, setAssetStates] = useState(null)
+    const [errorAssetStates, setErrorAssetStates] = useState(null)
+    let errorsFromForm = null
+    const [assetState, setAssetState] = useState([]) 
+    const [open, setOpen] = useState(showAlert)
+    const [assetStateId, setAssetStateId] = useState(0)
+    const [openToConfirm, setOpenToConfirm] = useState(false)
+    const [errorAssetStateDelete, setErrorAssetStateDelete] = useState(null)
+    const [errorCreateAssetState, setErrorCreateAssetState] = useState(null)
+    const [errorUpdateAssetState, setErrorUpdateAssetState] = useState(null)
+    
+    const [data, setData] = useState({
+        state:''//string
+    })
+
+    const [formErrors, setFormErrors] = useState({})
+    let assetStatesComponent = null   
+
+    function getAssetStates(){
+        axios.get(urlAssetStates).then(            
+            (res) => {
+                setAssetStates(res.data)
+            }
+        ).catch((e)=>{
+            setErrorAssetStates(e)
+        })
+    }
+
+    useEffect(() => {
+        axios.get(urlAssetStates).then(
+            res => setAssetStates(res.data)
+        ).catch((e)=>{
+            setErrorAssetStates(e)
+        })
+    }, [])
+    
+    const fetchDeleteAssetState = () => {    
+        axios.delete(urlAssetState + assetStateId)
+        .then(function (response) {
+            if (response.status == 200){
+                setShowAlert(true)
+                setAlertMessage("Registro Eliminado")
+                setSeverity("success")
+                setOpen(true)
+                setOpenToConfirm(false) 
+                getAssetStates()                                          
+            }
+        })
+        .catch(err=> {
+            setErrorAssetStateDelete(err)     
+            setOpenToConfirm(false)        
+        })
+    }
+
+    function hasFormErrors(errorsFromForm){
+        let hasErrors=true
+        if(!errorsFromForm.state){
+            hasErrors = false
+        }
+        return hasErrors
+    }
+
+    const validate = (datas) => {      
+        const errors = {
+            state: '' // string            
+        }        
+        if(!datas.state||datas.state.length==0)
+            errors.state= "El Estado es requerido!"
+        return errors     
+    }
+
+    const handleSave = ({name,value,previousValue},id) => {
+        if(value==previousValue || value=='') {
+            window.location.reload()
+        }      
+        else{
+            let updateData = {
+                state:value
+            }
+            submitUpdate(id,updateData)
+        }  
+        
+    }
+
+    function submitUpdate(id,updateData){
+        axios.put(urlAssetState + id, updateData).then((res) => {
+            if (res.status == 200) {               
+                setShowAlert(true)
+                setAlertMessage("Estado actualizado")
+                setSeverity("success")
+                setOpen(true)                    
+                getAssetStates()                    
+            }            
+        }).catch ((apiError) => {
+            setErrorUpdateAssetState(apiError)                    
+        })
+    }
+
+    function submitCreate(){
+        errorsFromForm = validate(data)
+        setFormErrors(errorsFromForm)
+        if(!hasFormErrors(errorsFromForm)){
+            axios.post(urlAssetState, data).then((res) => {
+                if (res.status == 201) {     
+                    setShowAlert(true)
+                    setAlertMessage("Estado creado")
+                    setSeverity("success")
+                    setOpen(true)
+                    getAssetStates()
+                    setData({
+                        state:''//string
+                    })
+                }            
+            }).catch ((apiError) => {
+                setErrorCreateAssetState(apiError) 
+                checkError()                    
+            })
+        }else{
+            console.log(errorsFromForm)
+        }
+    }
+    if (errorAssetStates) return ErrorPage(errorAssetStates)
+    if (errorAssetStateDelete){
+        if(errorAssetStateDelete.response.status==400 && errorAssetStateDelete.response.data=="El estado no puede ser eliminado porque existen activos fijos asociados a el."){
+            setShowAlert(true)
+            setAlertMessage(errorAssetStateDelete.response.data)
+            setSeverity("warning")
+            setOpen(true)
+            setErrorAssetStateDelete(null)
+        }
+        else
+            return ErrorPage(errorAssetStateDelete)
+    } 
+    if (errorCreateAssetState) return ErrorPage(errorCreateAssetState)
+    if (errorUpdateAssetState) return ErrorPage(errorUpdateAssetState)
+    if (!assetStates) return null    
+    if (!assetState)return <h1>ERROR: Estado de activo fijo no encontrado en la base de datos</h1>
+    const assetStatesListElements = assetStates.map((assetState)=>{
+        return {
+            id:assetState.id, 
+            title: assetState.state,     
+            description: ''       
+        }
+    })
+
+    function handleClose(event, reason) {
+        if (reason === 'clickaway') {            
+            return
+        }
+        setOpen(false)        
+    }
+
+    function handleCloseToConfirm(event, reason) {
+        if (reason === 'clickaway') {
+            return
+        }
+        setOpenToConfirm(false)
+    }
+    const ToConfirmOpen = () => {
+        handleCloseToConfirm();
+        setOpenToConfirm(true);
+    }
+
+    let deleteAction = (id) => {
+        setAssetStateId(id)          
+        handleCloseToConfirm()
+        ToConfirmOpen()
+    }         
+
+    function handle(e) {
+        const newData = { ...data }
+        newData[e.target.id] = e.target.value
+        setData(newData)
+        setOpen(false)
+    }
+    
+    assetStatesComponent = <ListGrid items={assetStatesListElements} withImage={false} editable={true} editActionOnSave={handleSave} withDeleteIcon={true} deleteAction={deleteAction}/>
+    return (        
+        <>        
+            <Navbar /><Box sx={{ display: 'flex', justifyContent: 'center' , marginTop:'15vh'}}>
+            {accesPermiss=="CompleteAccess"&&
+                <ListContainer title="Lista de Estados de Activos Fijos">
+                    {assetStatesComponent}
+                </ListContainer>
+            }   
+            </Box>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity={severity}>
+                    {alertMessage}                    
+                </Alert>
+            </Snackbar>
+            <Dialog open={openToConfirm} onClose={handleCloseToConfirm} id="confirmation_popup" sx={{borderRadius:3 }}>
+                <DialogTitle sx={{display:'flex', justifyContent:'center'}}>Eliminar</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        ¿Desea eliminar el estado?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions sx={{display:'flex',flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+                    <ButtonSecondary label="Cancelar" onClick={handleCloseToConfirm}></ButtonSecondary>
+                    <ButtonDanger label="Eliminar" id="confirm_delete_button" onClick={fetchDeleteAssetState}></ButtonDanger>
+                </DialogActions>
+            </Dialog>
+            <div style={{display:'flex', justifyContent:'center'}}>
+        <FormContainer title="Crear estado">
+            <InputText
+                required
+                id="state"
+                name="state"
+                value={data.state}
+                label="Estado"
+                type="text"
+                onChange={(e) => {
+                    handle(e)            
+                }}
+            />
+            {formErrors.state? <Alert  sx={{ wieditdth: 1, pt: 1 }} severity="error"> 
+                {formErrors.state}                   
+            </Alert>:<p></p> }
+            <ButtonPrimary label={"Crear estado"} id="submit_button" onClick={submitCreate}/>
+            </FormContainer>
+            </div>            
+        </>                
+    )
+}
---
+++ web-ui/src/Views/FixedAssets/AssetTypes/ShowAssetStates.spec.js
@@ -0,0 +1,100 @@
+import {render, screen, waitFor} from '@testing-library/react'
+import ShowAssetStates from './ShowAssetStates'
+import userEvent from '@testing-library/user-event'
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
+import {act} from 'react-dom/test-utils'
+
+import {MemoryRouter, Route, Routes} from 'react-router-dom'
+
+const getAssetStatesUrl ='https://ncv-api.azurewebsites.net/api/assetStates'  
+const oneAssetStatesUrl ='https://ncv-api.azurewebsites.net/api/assetStates/1'   
+
+const assetStates =
+  [
+    {
+        "id": 23,
+        "state": "Dañado",
+        "fixedAssets": []
+    },
+    {
+        "id": 5,
+        "state": "Verificar-estado",
+        "fixedAssets": []
+    },
+    {
+        "id": 20,
+        "state": "en proceso",
+        "fixedAssets": []
+    },
+    {
+        "id": 13,
+        "state": "Bueno",
+        "fixedAssets": []
+    },
+    {
+        "id": 3,
+        "state": "Malo",
+        "fixedAssets": []
+    },
+    {
+        "id": 2,
+        "state": "Regular",
+        "fixedAssets": []
+    }    
+]
+
+const assetStatesResponse = getResponse(getAssetStatesUrl, assetStates)
+const assetStateResponse = getResponse(oneAssetStatesUrl, assetStates[0])
+const handlers = [assetStatesResponse, assetStateResponse]
+const server = new setupServer(...handlers)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+function renderWithRouter(componentToRender, pathToElement, mockedPath){
+render( 
+    <MemoryRouter initialEntries={[mockedPath]}>
+        <Routes>
+            <Route path={pathToElement} element={componentToRender}></Route>
+        </Routes>
+    </MemoryRouter>
+)
+}
+
+function getResponse(url, jsonData=null, code=200, text=null){
+    const response = rest.get(url, (req, res, ctx) => {
+      if(code!=200) return res(ctx.status(code), ctx.text(text))
+      return res(ctx.json(jsonData))
+    })
+    return response
+}
+
+describe('CRUD asset states', () => {  
+   it('Shows all fixed asset states available', async () => {
+     act(()=>{
+       renderWithRouter(<ShowAssetStates/>,"/activos-fijos/estados","/activos-fijos/estados" )
+     }) 
+     await waitFor(() => {
+         expect(screen.queryByText('Lista de Estados de Activos Fijos')).toBeVisible
+         expect(screen.queryByText('Regular')).toBeVisible
+         expect(screen.queryByText('Malo')).toBeVisible
+         expect(screen.queryByText('en proceso')).toBeVisible
+         expect(screen.queryByText('Verificar-estado')).toBeVisible
+         expect(screen.queryByText('Dañado')).toBeVisible
+         expect(screen.queryByText('Bueno')).toBeVisible
+       })  
+   })
+   
+  it('Shows error when api does not return any data. Should return error 500', async () => {
+    const assetStatesInternalServiceErrorResponse = getResponse(getAssetStatesUrl, null, 500, "Lo sentimos, algo sucedió.")
+    server.use(assetStatesInternalServiceErrorResponse)
+    act(()=>{
+      renderWithRouter(<ShowAssetStates/>,"/activos-fijos/estados","/activos-fijos/estados" )
+    }) 
+    await waitFor(() => {
+        expect(screen.getByText("ERROR 500: Lo sentimos, algo sucedió.").toBeVisible)
+      })  
+  });
+})
