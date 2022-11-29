efe31462e09a14caab967fde082c37d8ac04af34
hmuyba <50967848+hmuyba@users.noreply.github.com>
2022-11-29 01:11:37 -0400

    functional search users

--- web-ui/src/Views/User/ListUsers.js
+++ web-ui/src/Views/User/ListUsers.js
@@ -1,27 +1,30 @@
 import ErrorPage from '../../Components/ErrorPage'
 import getFromApi from '../../Components/GetFromApi'
-import Alert from '@mui/material/Alert';
-import { Snackbar } from '@mui/material';
-import Box from '@mui/material/Box';
-import ListContainer from "../../Components/ListContainer";
-import Navbar from '../../Components/NavBar';
-import ButtonPrimary from '../../Components/MUI-Button';
+import Alert from '@mui/material/Alert'
+import { Snackbar } from '@mui/material'
+import Box from '@mui/material/Box'
+import ListContainer from '../../Components/ListContainer'
+import Navbar from '../../Components/NavBar'
+import ButtonPrimary from '../../Components/MUI-Button'
 import GutterList from '../../Components/GutterList'
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useState } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import TranslateRole from './Translate'
+import SearchBar from '../../Components/SearchBar'
+import { getListUsers } from './API/getAxios'
+var accesPermiss = sessionStorage.getItem("Access")
 
 function ListUsers() {
-    const url="https://ncv-api.azurewebsites.net/api/auth";
+    const url = 'https://ncv-api.azurewebsites.net/api/auth'
     //const url="http://localhost:5009/api/auth";
-    const { apiData:users, error } = getFromApi(url)
+    const { apiData: users, error } = getFromApi(url)
 
     const location = useLocation()
-    
-    let showAlert = location.state ? location.state.showAlert : false 
-    let alertMessage = location.state ? location.state.alertMessage : null 
-    const [open, setOpen] = useState(showAlert);
-
+    const [searchResult, setSearchResults] = useState([])
+    let showAlert = location.state ? location.state.showAlert : false
+    let alertMessage = location.state ? location.state.alertMessage : null
+    const [open, setOpen] = useState(showAlert)
+    const [usersList, setUsersList] = useState([])
     function handleClose(event, reason) {
         if (reason === 'clickaway') {
             return
@@ -29,44 +32,137 @@ function ListUsers() {
         setOpen(false)
     }
 
-   // const location = useLocation()
-    const completeInfoUser= "/vista-usuarios"
- 
+    function ordenCriteria(posts) {
+        posts = posts.sort((a, b) => {
+            return a.firstName
+                .toLowerCase()
+                .localeCompare(b.firstName.toLowerCase())
+        })
+        return posts
+    }
+
+    function searchCriteria(e, posts) {
+        if (!e.target.value) return posts
+        const resultsArray = posts.filter(
+            (post) =>
+                (
+                    post.firstName.toLowerCase() +
+                    ' ' +
+                    post.lastName.toLowerCase()
+                ).includes(e.target.value.toLowerCase()) ||
+                post.nameRole
+                    .toLowerCase()
+                    .includes(e.target.value.toLowerCase())
+        )
+        return resultsArray
+    }
+
+    useEffect(() => {
+        getListUsers()
+            .then((json) =>
+                json.sort((a, b) => {
+                    return a.firstName.localeCompare(b.firstName)
+                })
+            )
+            .then((json) => {
+                setUsersList(json)
+                return json
+            })
+            .then((json) => {
+                setSearchResults(json)
+            })
+    }, [])
+
+    // const location = useLocation()
+    const completeInfoUser = '/vista-usuarios'
 
     const navigate = useNavigate()
-    if(error){
+    if (error) {
         return ErrorPage(error)
     }
     if (!users) return null
-    if (users.length>0){
-        const listElements = users.map((el) => {
+
+
+    if (users.length > 0) {
+        const listElements = searchResult.map((el) => {
             return {
-                id:el.id, 
+                id: el.id,
                 title: `${el.firstName} ${el.lastName}`,
-                description: `${el.email} - ${el.cellPhone} - ${TranslateRole(el.nameRole)}`,
-                elementUrl:`${completeInfoUser}/${el.id}`,
-            };
+                description: `${el.email} - ${el.cellPhone} - ${TranslateRole(
+                    el.nameRole
+                )}`,
+                elementUrl: `${completeInfoUser}/${el.id}`
+            }
         })
-    
-        let usersComponents = <GutterList items={listElements} withImage={false} withDeleteIcon={true} />
-        let registerUser = "/registrarse-ncv"
-        let listHeaderComponents = <ButtonPrimary id="btn-register-user" label={"Registrar Usuario"} onClick={()=>navigate(registerUser)}/>
-        return ( 
+
+        let usersComponents = (
+            <GutterList
+                items={listElements}
+                withImage={false}
+                withDeleteIcon={true}
+            />
+        )
+        const searcher = (
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignSelf: 'right',
+                    justifyContent: 'center'
+                }}
+            >
+                <SearchBar
+                    posts={usersList}
+                    setSearchResults={setSearchResults}
+                    orderCriteria={ordenCriteria}
+                    searchCriteria={searchCriteria}
+                />
+            </Box>
+        )
+
+        let registerUser = '/registrarse-ncv'
+
+        const listHeaderComponents = (
+            <Box sx={{ display: 'flex' }}>
+                {searcher}
+                {accesPermiss == 'CompleteAccess' && (
+                    <ButtonPrimary
+                        id="btn-register-user"
+                        label={'Registrar Usuario'}
+                        onClick={() => navigate(registerUser)}
+                    />
+                )}
+            </Box>
+        )
+
+        return (
             <>
-                <Navbar /><Box sx={{ display: 'flex', justifyContent: 'center' , marginTop:'15vh'}}>
-                    <ListContainer title="Lista de usuarios" header={listHeaderComponents}>
+                <Navbar />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        marginTop: '15vh'
+                    }}
+                >
+                    <ListContainer
+                        title="Lista de usuarios"
+                        header={listHeaderComponents}
+                    >
                         {usersComponents}
-                        
                     </ListContainer>
                 </Box>
-                
-                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="success">
-                    {alertMessage}
-                </Alert>
-        </Snackbar>
+
+                <Snackbar
+                    open={open}
+                    autoHideDuration={6000}
+                    onClose={handleClose}
+                >
+                    <Alert onClose={handleClose} severity="success">
+                        {alertMessage}
+                    </Alert>
+                </Snackbar>
             </>
-        );
+        )
     }
 }
 export default ListUsers
