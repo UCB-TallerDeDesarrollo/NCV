d21b9bad905476acbfc7b26f986043b2a9d03e0f
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-11-29 09:36:45 -0400

    Campos requeridos

--- api/NinosConValorAPI/Models/ContactModel.cs
+++ api/NinosConValorAPI/Models/ContactModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NinosConValorAPI.Models
 {
     public class ContactModel
     {
+        [Required(ErrorMessage = "Nombre requerido")]
         public string Name { get; set; }
         public string Relationship { get; set; }
         public string? ContactNumber { get; set; }
