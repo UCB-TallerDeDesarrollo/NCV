34851c013ada35924067239dc63203c188360f7c
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-11-29 09:36:45 -0400

    Campos requeridos

--- api/NinosConValorAPI/Models/LegalReportModel.cs
+++ api/NinosConValorAPI/Models/LegalReportModel.cs
@@ -6,7 +6,9 @@ namespace NinosConValorAPI.Models
         public int Id { get; set; }
         public int KidId { get; set; }
         public string? CourtNumber { get; set; }
+        [Required(ErrorMessage = "DNA  required")]
         public string? Dna { get; set; }
+        [Required(ErrorMessage = "Title  required")]
         public string? Nurej { get; set; }
         public string? LegalProcesses { get; set; }
     }
