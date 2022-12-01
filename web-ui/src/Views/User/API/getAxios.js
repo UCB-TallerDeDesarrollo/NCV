4d2169288a9979c8ae4d607e20dbc0718f29e10c
hmuyba <50967848+hmuyba@users.noreply.github.com>
2022-12-01 10:07:23 -0400

    get userList fix

--- web-ui/src/Views/User/API/getAxios.js
+++ web-ui/src/Views/User/API/getAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://ncv-api.azurewebsites.net/api/kids"
+    baseURL: "https://ncv-api.herokuapp.com/"
 })
 
 export const getListUsers = async () => {
