80c66ee13de5b9ba8a22a0957bbfcdb0418b2eb8
CarolinaMelendez <venecia.melendez@ucb.edu.bo>
2022-12-08 12:05:54 -0400

    fixed edit family

--- web-ui/src/Views/KidsFiles/FamilyReport/EditFamilyReport.js
+++ web-ui/src/Views/KidsFiles/FamilyReport/EditFamilyReport.js
@@ -15,8 +15,10 @@ import { useState, useEffect } from 'react'
 import TextField from '@mui/material/TextField';
 
 const familyReport = {
-    siblingsInFoundation: null,
-    siblingsOutside: null,
+    siblingsInFoundation: '',
+    siblingsOutside: '',
+    hasExtendedFamily: '',
+    hasOriginFamily : ''
 }
 
 const booleansAnwers = [
@@ -85,12 +87,10 @@ function EditFamilyReport() {
     return (
         <><Navbar /><div style={{display:'flex', justifyContent:'center', marginTop: '3em'}}>
             <FormContainer title="Modificar reporte de familia">
-                <Collapse in={open} sx={{width:1, pt:2}}>
-                </Collapse>
                 <InputText
                     id="siblingsInFoundation"
                     name="siblingsInFoundation"
-                    
+                    label="Nro de Hermanos en el Centro"
                     type="number"
                     value={familyRep.siblingsInFoundation}
                     onChange={handleInputChange}
@@ -98,7 +98,7 @@ function EditFamilyReport() {
                 <InputText
                     id="siblingsOutside"
                     name="siblingsOutside"
-                   
+                    label="Nro de Hermanos externos"
                     type="number"
                     value={familyRep.siblingsOutside}
                     onChange={handleInputChange}
@@ -108,7 +108,7 @@ function EditFamilyReport() {
                     id="hasExtendedFamily"
                     name="hasExtendedFamily"
                     label="¿Tiene familia extendida?"
-                    value={familyReport_hasExtendedFamily}
+                    value={familyRep.hasExtendedFamily}
                     onChange={handleInputChange}
                 >
                 {booleansAnwers.map((option) => (
@@ -122,7 +122,7 @@ function EditFamilyReport() {
                     id="hasOriginFamily"
                     name="hasOriginFamily"
                     label="¿Tiene familia de origen?"
-                    value={familyReport_hasOriginFamily}
+                    value={familyRep.hasOriginFamily}
                     onChange={handleInputChange}
                 >
                 {booleansAnwers.map((option) => (
