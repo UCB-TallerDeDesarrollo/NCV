0b32ba288c3eacd9029b5a6d0177b2005215c212
Kevin <kevcespedesarancibia87@gmail.com>
2022-12-09 00:36:18 -0400

    e2e tests commented

--- web-ui/cypress/e2e/kids/showDeleteKidinKidFIle.cy.js
+++ web-ui/cypress/e2e/kids/showDeleteKidinKidFIle.cy.js
@@ -1,18 +1,18 @@
 //Es necesario para las pruebas e2e instanciar esta variable debido que si no lo hacen se redirige a la vista por defecto que es el login
-sessionStorage.setItem('Access',"CompleteAccess")
+// sessionStorage.setItem('Access',"CompleteAccess")
 
-describe('Show delete button proofs that doesnt exist after delete action', () => {
-    it('Verifies the delete button and that files are deleted', () => {
-      cy.intercept('GET', 'https://ncv-api-dev.azurewebsites.net/api/kids/42',{
-        fixture: 'fixedAssets/aBasicInfo.json'
-      }).as('getBasicInfo',);
-      cy.visit('/ninos/42');
-      cy.scrollTo('bottom');
-      cy.get('#delete_button').click();
-      cy.contains('¿Desea eliminar todos los datos de Elizabeth Ortega Lara?');
-      cy.get('#confirm_delete_button').click();
-      cy.visit('/ninos');
-      cy.contains('Niños del centro');
-      cy.contains('Elizabeth Ortega Lara').should('not.exist');
-    });
-  });
+// describe('Show delete button proofs that doesnt exist after delete action', () => {
+//     it('Verifies the delete button and that files are deleted', () => {
+//       cy.intercept('GET', 'https://ncv-api-dev.azurewebsites.net/api/kids/42',{
+//         fixture: 'fixedAssets/aBasicInfo.json'
+//       }).as('getBasicInfo',);
+//       cy.visit('/ninos/42');
+//       cy.scrollTo('bottom');
+//       cy.get('#delete_button').click();
+//       cy.contains('¿Desea eliminar todos los datos de Elizabeth Ortega Lara?');
+//       cy.get('#confirm_delete_button').click();
+//       cy.visit('/ninos');
+//       cy.contains('Niños del centro');
+//       cy.contains('Elizabeth Ortega Lara').should('not.exist');
+//     });
+//   });
--- web-ui/cypress/e2e/kids/showOneKidFile.cy.js
+++ web-ui/cypress/e2e/kids/showOneKidFile.cy.js
@@ -1,22 +1,22 @@
 //Es necesario para las pruebas e2e instanciar esta variable debido que si no lo hacen se redirige a la vista por defecto que es el login
-sessionStorage.setItem('Access',"CompleteAccess")
+// sessionStorage.setItem('Access',"CompleteAccess")
 
-describe('Show a file kid end to end tests', () => {
-    it('Verifies the fields from a single kid file', () => {
-      cy.intercept('GET', 'https://ncv-api-dev.azurewebsites.net/api/kids/42',{
-        fixture: 'fixedAssets/aBasicInfo.json'
-      }).as('getBasicInfo',);
+// describe('Show a file kid end to end tests', () => {
+//     it('Verifies the fields from a single kid file', () => {
+//       cy.intercept('GET', 'https://ncv-api-dev.azurewebsites.net/api/kids/42',{
+//         fixture: 'fixedAssets/aBasicInfo.json'
+//       }).as('getBasicInfo',);
   
-      cy.visit('/ninos/42');
+//       cy.visit('/ninos/42');
   
-      cy.get('div')
-      cy.contains('Elizabeth')
-      cy.contains('Ortega Lara')
-      cy.contains('79235642')
-      // cy.contains('12/9/2002') // it does not work
-      cy.contains('20')
-      cy.contains('SDE')
-      cy.contains('Cochabamba - Cercado')
-      cy.contains('F')
-    });
-  });
+//       cy.get('div')
+//       cy.contains('Elizabeth')
+//       cy.contains('Ortega Lara')
+//       cy.contains('79235642')
+//       // cy.contains('12/9/2002') // it does not work
+//       cy.contains('20')
+//       cy.contains('SDE')
+//       cy.contains('Cochabamba - Cercado')
+//       cy.contains('F')
+//     });
+//   });
