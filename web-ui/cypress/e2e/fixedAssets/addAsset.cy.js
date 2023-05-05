88e4323e3a6074cf3356bab23a5f0c4f6b52c314
Alfred <alfred.garcia@ucb.edu.bo>
2023-05-05 13:22:48 -0400

    REFACTORING de Crear activo fijo ... cypress

--- web-ui/cypress/e2e/fixedAssets/addAsset.cy.js
+++ web-ui/cypress/e2e/fixedAssets/addAsset.cy.js
@@ -1,39 +1,72 @@
+/// <reference types="Cypress" />
+
 sessionStorage.setItem('Access',"CompleteAccess")
 
-describe('Add a fixed asset end to end tests', () => {
-    it('Creates a fixed asset', () => {
-      cy.intercept('GET', process.env.REACT_APP_BACKEND_URL + '/api/programHouses',{
-        fixture: 'programHouses/listOfProgramHouses.json',
-        statusCode: 200
-      }).as('programHouses',)
-
+describe(' Agregar pruebas de extremo a extremo de activos fijos ', () => {
+    it(' Crea un activo fijo (HAPPY PATH) ', () => {
       cy.visit('/crear-activo-fijo')  
-      //cy.wait('@programHouses').its('response.statusCode').should('equal', 200)
+
       cy.get('#Name').type('Principito',{force: true})
+      cy.wait(1000)
 
-      //cy.get('#category-drop').select([1])
-      //cy.get('#category-drop').select("data-value=1")
-      //cy.get('#category-drop').type('Maquinaria y Equipos',{force: true})
-      cy.get('.MuiListItemText-primary').contains('Maquinaria y Equipos').click()
+      cy.get('#category-drop').click({force: true})
+      cy.get("li[role='option']").each(function ($ele, index, list) {
+        //cy.log($ele.text())
+        if ($ele.text() === 'Maquinaria y Equipos') {
+          cy.log("Elemento encontrado")
+          $ele.wrap($ele).click()
+        }
+        else {
+          cy.log("Valores restantes ", $ele.text())
+        }
+      })
+      cy.wait(1000)
 
-      cy.get('#type-drop').type('Libro',{force: true})
+      cy.get('#type-drop').click({force: true})
+      cy.get("[role='option']").each(function ($ele, index, list) {
+        if ($ele.text() === 'Impresora') {
+          $ele.wrap($ele).click()
+        }
+      })
+      cy.wait(1000)
+      
       cy.get('#Price').type(50,{force: true})
-      cy.get('#programa-drop').type('CRE',{force: true})
-      cy.get('#estado-drop').type('En uso',{force: true})
-      cy.get('#responsable-drop').type('Andres Peredo',{force: true})
+      cy.wait(1000)
+
+      cy.get('#programa-drop').click({force: true})
+      cy.get("li[role='option']").each(function ($ele, index, list) {
+        if ($ele.text() === 'CRE') {
+          $ele.wrap($ele).click()
+        }
+      })
+      cy.wait(1000)
+
+      cy.get('#estado-drop').click({force: true})
+      cy.get("li[role='option']").each(function ($ele, index, list) {
+        if ($ele.text() === 'Activo') {
+          $ele.wrap($ele).click()
+        }
+      })
+      cy.wait(1000)
+    
+      cy.get('#responsable-drop').click({force: true})
+      cy.get("li[role='option']").each(function ($ele, index, list) {
+        if ($ele.text() === 'Jose Arebalo') {
+          $ele.wrap($ele).click()
+        }
+      })
+      cy.wait(1000)
+
       cy.get('#Location').type('Cochabamba',{force: true})
+      cy.wait(1000)
+
       cy.get('#Code').type('F-CRE-MAQ-0913',{force: true})
+      cy.wait(1000)
   
       cy.get('#submit_button').click()
       cy.clock()
-      
-      /*
-      cy.intercept('POST', process.env.REACT_APP_BACKEND_URL + '/api/fixedAssets',{
-        fixture: 'fixedAssets/listOfAssets.json',
-        statusCode: 201
-      }).as('anAssetPost',)
-      cy.wait('@anAssetPost').its('response.statusCode').should('equal', 201)
-      */
 
+      cy.visit('/activos-fijos')
+      cy.log('CREACION EXITOSA !')
     });
   });
