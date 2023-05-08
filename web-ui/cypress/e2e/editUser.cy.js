c84a7fd232e6f1ed83b17b19b3185f6c83fe334c
LuisSebastian28 <sabestech@gmail.com>
2023-05-07 20:52:46 -0400

    pruebas e2e capturando llamadas a la api

--- web-ui/cypress/e2e/editUser.cy.js
+++ web-ui/cypress/e2e/editUser.cy.js
@@ -1,168 +1,131 @@
+sessionStorage.setItem('Access', "CompleteAccess")
+
 describe('Edit users end to end tests', () => {
-var rol = 'Administrador'
-  beforeEach(() => {
-    const email = 'soporteNCV@gmail.com'
-    const password = 'Soporte!23'
-   
-    sessionStorage.setItem('Access','');
-    cy.login(email, password)
-  });
-  
-  it('Crear usuario Test User', () => {
-    cy.viewport(1200, 1200);
+  it('Verificar happy path (cambio de numero)', () => {
     const randomNumber = Math.floor(Math.random() * 100);
-  
-    cy.get('button').contains('Usuarios').click();
-    cy.get('button').contains('Registrar Usuario').click();
-    
-    cy.get('#firstName', {force: true})
-      .type('Test');
-    
-    cy.get('#lastName').type('User');   
-    cy.get('#cellPhone').type(randomNumber);
-    cy.get('#email').type('testexample@gmail.com');
-    
-    cy.get('#rol').click();
-    cy.contains('li', rol).click();
-    
-    cy.get('button[type="input"][label="Registrar"]').click({force: true});
-    cy.get('.MuiListItemText-primary').contains(rol).click()
-    cy.get('ul.MuiList-root')
-      .contains('Test User')
-      .should('be.visible');
 
-  });
-  
+    cy.intercept('GET', 'https://ncv-api-staging.azurewebsites.net/api/auth/Sebas', {
+      fixture: 'Users/testUser.json'
+    }).as('getBasicInfo',);
+    cy.intercept('PUT', 'https://ncv-api-staging.azurewebsites.net/api/auth/Sebas', {
+      "email": "testUser@gmail.com",
+      "cellPhone": randomNumber,
+      "firstName": "User",
+      "lastName": "Test",
+      "role": "Administrador",
+      "rol": "Administrador"
+    }).as('getBasicInfo',);
 
-   
-   it('Verificar happy path (cambio de numero)', () => {
-     const randomNumber = Math.floor(Math.random() * 100);
-
-     cy.get('button').contains('Usuarios').click()
-     cy.get('.MuiListItemText-primary').contains(rol).click()
-
-     cy.contains('li', 'Test User')
-     .within(() => {
-       cy.get('.editar-assetState-button').click();
-     })
-     cy.wait(3000);
-
-     cy.get('#cellPhone')
-     .clear()
-     .type(randomNumber)
-
-     cy.get('button[type="input"][label="Guardar Cambios"]').click();
-     cy.get('.MuiListItemText-primary').contains(rol).click()
-     
-     cy.contains('li', 'Test User')
-     .parent()
-     .within(() => {
-       cy.contains(randomNumber).should('be.visible');
-     });
-
-   })
-
-   it('Verificar happy path (sin cambiar ningun dato del usuario)', () => {
-    cy.get('button').contains('Usuarios').click()
-    cy.get('.MuiListItemText-primary').contains(rol).click()
-
-    cy.contains('li', 'Test User')
-    .within(() => {
-      cy.get('.editar-assetState-button').click();
-    })
-    cy.wait(10000);
-
+    cy.intercept('GET', 'https://ncv-api-staging.azurewebsites.net/api/auth', [
+      {
+        "email": "testUser@gmail.com",
+        "firstName": "User",
+        "lastName": "Test",
+        "cellPhone": randomNumber,
+        "nameRole": "Administrador",
+        "id": "Sebas"
+      }
+    ]).as('getBasicInfo',);
+    cy.visit('/vista-usuarios/Sebas');
+    cy.get('#cellPhone')
+      .clear()
+      .type(randomNumber)
     cy.get('button[type="input"][label="Guardar Cambios"]').click();
-
-    cy.get('.MuiListItemText-primary').contains(rol).click()
-
-    cy.wait(10000);
-    
+    cy.get('.MuiListItemText-primary').contains('Administrador').click()
     cy.get('ul.MuiList-root')
-      .contains('Test User')
+      .contains(randomNumber)
       .should('be.visible');
-  })
+  });
 
   it('Verificar happy path (cambio de rol)', () => {
-    cy.get('button').contains('Usuarios').click()
-    cy.get('.MuiListItemText-primary').contains(rol).click()
+    cy.intercept('GET', 'https://ncv-api-staging.azurewebsites.net/api/auth/Sebas', {
+      fixture: 'Users/testUser.json'
+    }).as('getBasicInfo',);
+    cy.intercept('PUT', 'https://ncv-api-staging.azurewebsites.net/api/auth/Sebas', {
+      "email": "testUser@gmail.com",
+      "cellPhone": "7777777",
+      "firstName": "User",
+      "lastName": "Test",
+      "role": "Administrador",
+      "rol": "Administrador"
+    }).as('getBasicInfo',);
 
-    cy.contains('li', 'Test User')
-    .within(() => {
-      cy.get('.editar-assetState-button').click();
-    })
-    cy.wait(5000);
-
+    cy.intercept('GET', 'https://ncv-api-staging.azurewebsites.net/api/auth', [
+      {
+        "email": "testUser@gmail.com",
+        "firstName": "User",
+        "lastName": "Test",
+        "cellPhone": "7777777",
+        "nameRole": "Soporte",
+        "id": "Sebas"
+      }
+    ]).as('getBasicInfo',);
+    cy.visit('/vista-usuarios/Sebas');
     cy.get('#rol').click();
     cy.contains('li', 'Equipo Tecnico').click();
-    rol = 'Equipo Tecnico'
-    cy.get('button[type="input"][label="Guardar Cambios"]').click({force: true});
-
-    cy.get('.MuiListItemText-primary').contains(rol).click()
-
-    cy.wait(10000);
-
+    cy.get('button[type="input"][label="Guardar Cambios"]').click({ force: true });
+    cy.get('.MuiListItemText-primary').contains('Soporte').click()
     cy.get('ul.MuiList-root')
-      .contains('Test User')
+      .contains('User Test')
       .should('be.visible');
-  })
+  });
 
+  it('Verificar happy path (sin cambiar nada)', () => {
+    cy.intercept('GET', 'https://ncv-api-staging.azurewebsites.net/api/auth/Sebas', {
+      fixture: 'Users/testUser.json'
+    }).as('getBasicInfo',);
+    cy.intercept('PUT', 'https://ncv-api-staging.azurewebsites.net/api/auth/Sebas', {
+      "email": "testUser@gmail.com",
+      "cellPhone": "7777777",
+      "firstName": "User",
+      "lastName": "Test",
+      "role": "Administrador",
+      "rol": "Administrador"
+    }).as('getBasicInfo',);
 
-  it('Eliminar Test User', () => {
-
-    cy.get('button').contains('Usuarios').click()
-    cy.get('.MuiListItemText-primary').contains(rol).click()
-
-    cy.contains('li', 'Test User')
-      .within(() => {
-    cy.get('[title="Eliminar"]').click();
-    });
-    cy.get('button[type="input"][label="Eliminar"]').click({force: true});
-    cy.wait(3000);
-
-    cy.get('.MuiListItemText-primary').contains(rol).click({force: true});
+    cy.intercept('GET', 'https://ncv-api-staging.azurewebsites.net/api/auth', [
+      {
+        "email": "testUser@gmail.com",
+        "firstName": "User",
+        "lastName": "Test",
+        "cellPhone": "7777777",
+        "nameRole": "Administrador",
+        "id": "Sebas"
+      }
+    ]).as('getBasicInfo',);
+    cy.visit('/vista-usuarios/Sebas');
+    cy.get('button[type="input"][label="Guardar Cambios"]').click({ force: true });
+    cy.get('.MuiListItemText-primary').contains('Administrador').click()
     cy.get('ul.MuiList-root')
-      .should('not.contain', 'Test User');
-  })
+      .contains('User Test')
+      .should('be.visible');
+  });
 
-
-   /*
-   
-
-   
-
-   it('Validar mensaje de error al dejar vacio el numero', () => {
-     cy.get('button').contains('Usuarios').click()
-     cy.get('.MuiListItemText-primary').contains('Administrador').click()
-
-     cy.contains('li', 'Test User')
-     .within(() => {
-       cy.get('.editar-assetState-button').click();
-     })
-     cy.wait(10000);
-     cy.get('#cellPhone')
-     .clear()
-     cy.get('button[type="input"][label="Guardar Cambios"]').click();
-     cy.get('.MuiAlert-message').should('have.text', 'Todos los campos son requeridosEl celular es requerido!');
-
-   })
-
-   it('Validar mensaje de error al dejar vacio el correo', () => {
-     cy.get('button').contains('Usuarios').click()
-     cy.get('.MuiListItemText-primary').contains('Administrador').click()
-
-     cy.contains('li', 'Test User')
-     .within(() => {
-       cy.get('.editar-assetState-button').click();
-     })
-     cy.wait(10000);
-     cy.get('#email')
-     .clear()
-     cy.get('button[type="input"][label="Guardar Cambios"]').click();
-     cy.get('.MuiAlert-message').should('have.text', 'Todos los campos son requeridosEl correo es requerido!');
-
-   })*/
-
-   
+  it('Verificar error campos vacios', () => {
+    cy.intercept('GET', 'https://ncv-api-staging.azurewebsites.net/api/auth/Sebas', {
+      fixture: 'Users/testUser.json'
+    }).as('getBasicInfo',);
+    cy.intercept('PUT', 'https://ncv-api-staging.azurewebsites.net/api/Sebas', {
+      "type": "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+      "title": "One or more validation errors occurred.",
+      "status": 400,
+      "traceId": "00-f6c73416d294ae9e9159f6e0b69768e8-27022178a7dc3d7f-00",
+      "errors": {
+        "Email": [
+          "The Email field is not a valid e-mail address."
+        ],
+        "CellPhone": [
+          "The CellPhone field is not a valid phone number."
+        ]
+      }
+    }).as('getBasicInfo',);
+    cy.visit('/vista-usuarios/Sebas');
+    cy.get('#cellPhone').clear()
+    cy.get('#firstName').clear()
+    cy.get('#lastName').clear()
+    cy.get('#email').clear()
+    cy.get('button[type="input"][label="Guardar Cambios"]').click();
+    cy.get('.MuiAlert-message').should('have.text', 'Todos los campos son requeridosEl nombre es requerido!El apellido es requerido!El celular es requerido!El correo es requerido!');
+  });
 });
 
