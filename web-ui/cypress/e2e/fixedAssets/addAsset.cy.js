827bd9898b7d5e77986e63330d0cdfd8dbf2c5d5
Alfred <alfred.garcia@ucb.edu.bo>
2023-05-09 10:11:37 -0400

    Hot Fix

--- web-ui/cypress/e2e/fixedAssets/addAsset.cy.js
+++ web-ui/cypress/e2e/fixedAssets/addAsset.cy.js
@@ -2,121 +2,72 @@
 
 sessionStorage.setItem('Access',"CompleteAccess")
 
-describe(' Agregar pruebas de extremo a extremo de ACTIVOS FIJOS ', () => {
+describe(' AGREGAR PRUEBAS DE EXTREMO A EXTREMO DE ACTIVOS FIJOS ', () => {
+  beforeEach( function() {
+    cy.visit('/crear-activo-fijo')
+  })
 
-    it(' Crea un activo fijo (HAPPY PATH) ', () => {
-      cy.visit('/crear-activo-fijo')  
+    it(' Crear un activo fijo (HAPPY PATH) ', () => {
+      cy.intercept('POST', 'https://ncv-api-staging.azurewebsites.net/api/fixedAssets', {
+          fixture: 'fixedAssets/aBasicInfo.json'
+      }).as('getBasicInfo-Fixture')
 
       cy.get('#Name').type('Principito',{force: true})
-      cy.wait(1000)
-
       cy.get('#category-drop').click({force: true})
       cy.get("li[role='option']").each(function ($ele) {
-        //cy.log($ele.text())
         if ($ele.text() === 'Material Escolar') {
-          cy.log("Elemento encontrado")
           $ele.wrap($ele).click()
         }
-        else {
-          cy.log("Valores restantes ", $ele.text())
-        }
       })
-      cy.wait(1000)
-
       cy.get('#type-drop').click({force: true})
-      cy.get("[role='option']").each(function ($ele) {
+      cy.get("li[role='option']").each(function ($ele) {
         if ($ele.text() === 'Libro') {
           $ele.wrap($ele).click()
         }
       })
-      cy.wait(1000)
-      
       cy.get('#Price').type(50,{force: true})
-      cy.wait(1000)
-
       cy.get('#programa-drop').click({force: true})
       cy.get("li[role='option']").each(function ($ele) {
         if ($ele.text() === 'CRE') {
           $ele.wrap($ele).click()
         }
       })
-      cy.wait(1000)
-
       cy.get('#estado-drop').click({force: true})
       cy.get("li[role='option']").each(function ($ele) {
         if ($ele.text() === 'Descompuesto') {
           $ele.wrap($ele).click()
         }
       })
-      cy.wait(1000)
-    
       cy.get('#responsable-drop').click({force: true})
       cy.get("li[role='option']").each(function ($ele) {
         if ($ele.text() === 'Pepito Perez') {
           $ele.wrap($ele).click()
         }
       })
-      cy.wait(1000)
-
-      cy.get('#Location').type('Cochabamba',{force: true})
-      cy.wait(1000)
-
+      cy.get('#Location').type('Distrito X',{force: true})
       cy.get('#Code').type('F-CRE-MAQ-0913',{force: true})
-      cy.wait(1000)
-  
       cy.get('#submit_button').click()
       cy.clock()
-      //cy.pause()
     });
+  });
+     
 
-    it(' Validar mensaje de error al dejar vacio el Detalle (Obligatorio) ', () => {
-      cy.visit('/crear-activo-fijo')
-      cy.get('#Name').should('have.value', '');
-
-      /*
-      cy.get("[role='alert']").each(function ($ele) {
-        if ($ele.text() === 'El Detalle del Activo Fijo es requerido!') {
-          $ele.wrap($ele).click()
-        }
+  describe(' VALIDAR MENSAJE DE ERROR AL DEJAR VACIO EL CAMPO OBLIGATORIO ', () => {
+      beforeEach( function() {
+        cy.visit('/crear-activo-fijo')
       })
-      */
 
-      cy.get('#submit_button').click()
-      cy.clock()
-    });
+      it(' Deberia validar mensaje de error en Detalle, Valor y Codigo ', () => {
+        cy.intercept('POST', 'https://ncv-api-staging.azurewebsites.net/api/fixedAssets', {
+            fixture: 'fixedAssets/aBasicInfo.json'
+        }).as('getBasicInfo-Fixture')
 
-    it(' Validar mensaje de error al dejar vacio el Valor (Obligatorio) ', () => {
-      cy.visit('/crear-activo-fijo')
-      cy.get('#Price').should('have.value', '');
-
-      /*
-      cy.get("[role='alert']").each(function ($ele) {
-        if ($ele.text() === 'El Detalle del Activo Fijo es requerido!') {
-          $ele.wrap($ele).click()
-        }
-      })
-      */
-
-      cy.get('#submit_button').click()
-      cy.clock()
-      cy.wait(5000)
-    });
-
-    it(' Validar mensaje de error al dejar vacio el Codigo (Obligatorio) ', () => {
-      cy.visit('/crear-activo-fijo')
-      cy.get('#Code').should('have.value', '');
-
-      /*
-      cy.get("[role='alert']").each(function ($ele) {
-        if ($ele.text() === 'El Detalle del Activo Fijo es requerido!') {
-          $ele.wrap($ele).click()
-        }
-      })
-      */
-
-      cy.get('#submit_button').click()
-      cy.clock()
-      cy.wait(5000)
-      
-    });
+        cy.get('#Name').should('have.value', '');
+        cy.get('#submit_button').click()
+        cy.get('#Price').should('have.value', '');
+        cy.get('#submit_button').click()
+        cy.get('#Code').should('have.value', '');
+        cy.get('#submit_button').click()
+        cy.clock()
+      });
   });
