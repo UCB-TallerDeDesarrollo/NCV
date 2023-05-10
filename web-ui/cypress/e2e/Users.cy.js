68b113d2c4072da55acb49d82b933d7e2cf0b8e5
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2023-05-10 12:56:49 -0400

    implementacion de pruebas e2e para accesos de usuarios y adicion de los nombres de las clases en navbar

--- web-ui/cypress/e2e/Users.cy.js
+++ web-ui/cypress/e2e/Users.cy.js
@@ -1,25 +1,123 @@
-// describe('Home Page end to end tests', () => {
+describe('pruebas accesos usuarios', () => {
+    const email = 'soporteNCV@gmail.com'
+    const password = 'Soporte!23'
+  
+     beforeEach(() => {
+     cy.visit('/');
+     
+   });
+  
+  
+   // Rol de Admin
+// asturizaga99@gmail.com
+// 9#Dc7^Ja
+// Rol de Tia
+// danocito.lol@gmail.com
+// 8^Eg9&Hj
+// Rol de Tecnico
+// tecncv@gmail.com
+// Tecnico!23
+  
+it('Deberia mostrar solo los botones a los que tiene acceso el usuario ADMINISTRADOR', () => {
+
+  cy.get('#input-text-email').type("asturizaga99@gmail.com");
+  cy.get('#input-text-password').type("9#Dc7^Ja");
     
-//   beforeEach(() => {
-//     const email = 'soprteNCV@gmail.com'
-//     const password = 'Soporte!23'
+     
+  cy.get('#input-button-login').click();
+      
+  cy.url().should('include', 'inicio-ncv');
+  cy.visit('/inicio-ncv');
 
-//     sessionStorage.setItem('Access','');
-//     cy.login(email, password);
+  cy.get('button')
+      .should('have.class', 'btn-files')
+      .and('contain', 'Niños');
+  cy.get('button')
+      .should('have.class', 'btn-activosFijos')
+      .and('contain', 'Activos Fijos');
+  cy.get('button')
+      .should('have.class', 'btn-users')
+      .and('contain', 'Usuarios');
+  cy.get('button')
+      .should('have.class', 'btn-exit')
+      .and('contain', 'Salir');
+});  
 
-//     cy.get('.btn-users').click();
-//     cy.url().should('include', 'vista-usuarios');
-//   });
+it('Deberia mostrar solo los botones a los que tiene acceso el usuario TIA', () => {
 
-//   it('Se muestran la vista de usuarios', () => {
-//     cy.get('#btn-register-user').should('contain', 'Registrar Usuario');
+      cy.get('#input-text-email').type("danocito.lol@gmail.com");
+      cy.get('#input-text-password').type("8^Eg9&Hj");
+        
+         
+      cy.get('#input-button-login').click();
+          
+      cy.url().should('include', 'inicio-ncv');
+      cy.visit('/inicio-ncv');
+  
+      cy.get('button')
+          .should('have.class', 'btn-files')
+          .and('contain', 'Niños');
 
-//     
-//   });
+      cy.get('button')
+          .should('have.class', 'btn-perfil')
+          .and('contain', 'Perfil');
 
-//   it('Se redirije a la vista de registro de usuario', () => {
-//     cy.get('#btn-register-user').click({force: true});
+      cy.get('button')
+          .should('have.class', 'btn-exit')
+          .and('contain', 'Salir');
+});    
 
-//     cy.url().should('include', 'registrarse-ncv');
-//   });
-// });
+  it('Deberia mostrar solo los botones a los que tiene acceso el usuario TECNICO', () => {
+
+      cy.get('#input-text-email').type("sebas_ag97@hotmail.com");
+      cy.get('#input-text-password').type("9#Dc7^Ja");
+             
+      cy.get('#input-button-login').click();
+              
+      cy.url().should('include', 'inicio-ncv');
+      cy.visit('/inicio-ncv');
+      
+      cy.get('button')
+        .should('have.class', 'btn-files')
+        .and('contain', 'Niños');
+  
+      cy.get('button')
+        .should('have.class', 'btn-perfil')
+        .and('contain', 'Perfil');
+  
+      cy.get('button')
+      .should('have.class', 'btn-exit')
+      .and('contain', 'Salir');
+  });
+
+  it('Deberia mostrar solo los botones a los que tiene acceso el usuario SOPORTE', () => {
+
+      cy.get('#input-text-email').type(email);
+      cy.get('#input-text-password').type(password);
+             
+      cy.get('#input-button-login').click();
+              
+      cy.url().should('include', 'inicio-ncv');
+      cy.visit('/inicio-ncv');
+      
+      cy.get('button')
+          .should('have.class', 'btn-files')
+          .and('contain', 'Niños');
+      cy.get('button')
+          .should('have.class', 'btn-activosFijos')
+          .and('contain', 'Activos Fijos');
+      cy.get('button')
+          .should('have.class', 'btn-users')
+          .and('contain', 'Usuarios');
+      cy.get('button')
+          .should('have.class', 'btn-exit')
+          .and('contain', 'Salir');
+  
+      cy.get('button')
+          .should('have.class', 'btn-perfil')
+          .and('contain', 'Perfil');
+  });
+  
+  
+  
+  });
