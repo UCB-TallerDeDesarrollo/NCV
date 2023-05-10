99cf8d093a8daf3f09130a73ea28d15851de5bf8
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2023-05-10 12:44:41 -0400

    implementacion pruebas e2e para login

--- web-ui/cypress/e2e/Login.cy.js
+++ web-ui/cypress/e2e/Login.cy.js
@@ -1,5 +1,5 @@
 describe('Login', () => {
-    const email = 'soporteNCV2@gmail.com'
+    const email = 'soporteNCV@gmail.com'
     const password = 'Soporte!23'
   
      beforeEach(() => {
@@ -7,56 +7,53 @@ describe('Login', () => {
      
    });
   
-//    it('muestra la pagina de LogIn', () => {
-//          cy.contains('Ingresa con tu cuenta');
-//   });
+   it('muestra la pagina de LogIn', () => {
+         cy.contains('Ingresa con tu cuenta');
+  });
   
-//    it('bloquea rutas protegidas', () => {
+   it('bloquea rutas protegidas', () => {
   
-//      cy.visit('/inicio-ncv');
+     cy.visit('/inicio-ncv');
   
     
-//     cy.url().should('include', '/');
+    cy.url().should('include', '/');
   
-//    })
+   })
   
-//    it('Deberia iniciar sesion con la cuenta y contraseña correctos', () => {
+   it('Deberia iniciar sesion con la cuenta y contraseña correctos', () => {
     
-//     cy.clock()
-//     cy.get('#input-text-email').type(email);
-//     cy.get('#input-text-password').type(password);
+    cy.clock()
+    cy.get('#input-text-email').type(email);
+    cy.get('#input-text-password').type(password);
   
-//     cy.tick(10000)
-//     cy.get('#input-button-login').click();
+    cy.tick(10000)
+    cy.get('#input-button-login').click();
   
-//     cy.url().should('include', 'inicio-ncv');
-//   });
+    cy.url().should('include', 'inicio-ncv');
+  });
   
-//   it('Deberia mostrar un mensaje de error al intentar ingresar con la contraseña incorrecta', () => {
-//   cy.get('#input-text-email').type(email);
-//   cy.get('#input-text-password').type("1234");
+  it('Deberia mostrar un mensaje de error al intentar ingresar con la contraseña incorrecta', () => {
+  cy.get('#input-text-email').type(email);
+  cy.get('#input-text-password').type("1234");
   
-//   cy.get('#input-button-login').click();
-  
-  
-//   cy.get('#alert-bad-user').children()
-//   .should('contain', 'Usuario y/o contraseña no validos')
-//   .and('be.visible')
-//   })
-  
-//   it('Deberia mostrar un mensaje de error al intentar ingresar con el email incorrecto', () => {
-//     cy.get('#input-text-email').type("soporte");
-//    cy.get('#input-text-password').type(password);
-  
-//    cy.get('#input-button-login').click();
+  cy.get('#input-button-login').click();
   
   
-//     cy.get('#alert-bad-user').children()
-//       .should('contain', 'Usuario y/o contraseña no validos')
-//       .and('be.visible')
-//    })
+  cy.get('#alert-bad-user').children()
+  .should('contain', 'Usuario y/o contraseña no validos')
+  .and('be.visible')
+  })
+  
+  it('Deberia mostrar un mensaje de error al intentar ingresar con el email incorrecto', () => {
+    cy.get('#input-text-email').type("soporte");
+   cy.get('#input-text-password').type(password);
+  
+   cy.get('#input-button-login').click();
   
   
-  
+    cy.get('#alert-bad-user').children()
+      .should('contain', 'Usuario y/o contraseña no validos')
+      .and('be.visible')
+   }) 
   
   });
