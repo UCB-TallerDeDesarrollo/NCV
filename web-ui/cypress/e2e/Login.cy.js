708481a239849a541355a4736a1010f52f5ff47e
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2023-06-01 02:19:29 -0400

    mejorar la presentacion del codigo para evitar errores

--- web-ui/cypress/e2e/Login.cy.js
+++ web-ui/cypress/e2e/Login.cy.js
@@ -8,54 +8,34 @@ describe('Login', () => {
 
    it('muestra la pagina de LogIn', () => {
     cy.visit(urlVisit);
-         cy.contains('Ingresa con tu cuenta');
+    cy.contains('Ingresa con tu cuenta');
   });
   
    it('bloquea rutas protegidas', () => {
-  
-     cy.visit(urlVisit);
-  
-    
+    cy.visit(urlVisit);
     cy.url().should('include', '/');
-  
    })
   
    it('Deberia iniciar sesion con la cuenta y contraseña correctos', () => {
-    
     cy.visit(urlVisit);
     cy.clock()
     cy.get('#input-text-email').type(email);
     cy.get('#input-text-password').type("Soporte123!");
-  
-
     cy.get('#input-button-login').click();
-    cy.get('button')
-          .should('have.class', 'btn-files')
-          .and('contain', 'Niños');
+    cy.get('button').should('have.class', 'btn-files').and('contain', 'Niños');
   });
   
   it('Deberia mostrar un mensaje de error al intentar ingresar con la contraseña incorrecta', () => {
-  cy.get('#input-text-email').type(email);
-  cy.get('#input-text-password').type("1234");
-  
-  cy.get('#input-button-login').click();
-  
-  
-  cy.get('#alert-bad-user').children()
-  .should('contain', 'Usuario y/o contraseña no validos')
-  .and('be.visible')
+    cy.get('#input-text-email').type(email);
+    cy.get('#input-text-password').type("1234");
+    cy.get('#input-button-login').click();
+    cy.get('#alert-bad-user').children().should('contain', 'Usuario y/o contraseña no validos').and('be.visible');
   })
   
   it('Deberia mostrar un mensaje de error al intentar ingresar con el email incorrecto', () => {
     cy.get('#input-text-email').type("soporte");
-   cy.get('#input-text-password').type(password);
-  
-   cy.get('#input-button-login').click();
-  
-  
-    cy.get('#alert-bad-user').children()
-      .should('contain', 'Usuario y/o contraseña no validos')
-      .and('be.visible')
+    cy.get('#input-text-password').type(password);
+    cy.get('#input-button-login').click();
+    cy.get('#alert-bad-user').children().should('contain', 'Usuario y/o contraseña no validos').and('be.visible');
    }) 
-  
   });
