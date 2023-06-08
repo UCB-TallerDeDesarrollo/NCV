6831ecf2b90129423b532add02a22fc07f9e74ef
Alfred <alfred.garcia@ucb.edu.bo>
2023-06-08 09:06:39 -0400

    Formato e inicio de pruebas

---
+++ web-ui/cypress/e2e/fixedAssets/addAssetShortCategory.cy.js
@@ -0,0 +1,11 @@
+sessionStorage.setItem('Access', "CompleteAccess")
+describe(' Crear las pruebas de extremo a extremo de Categoria de Codigo Corto ', () => {
+  
+  it(' Verificar happy path (EDITAR) y codigo de estado ', () => {
+    
+  });
+
+  it(' Deberia mostrar un mensaje que no se puede dejar el campo de edicion vacio ', () => {
+    
+  });
+});
